03d039c82a4445a9acff4b84158671a71c73f859
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2006-11-20 10:31:37 +0000

    added missing file
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@434 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ include/qore/ql_json.h
@@ -0,0 +1,31 @@
+/*
+  ql_json.h
+
+  qore JSON support functions
+
+  Qore Programming Language
+
+  Copyright (C) 2004, 2005, 2006 David Nichols
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef _QORE_QL_JSON_H
+
+#define _QORE_QLJSON_H
+
+void init_json_functions();
+
+#endif // _QORE_QL_JSON_H
