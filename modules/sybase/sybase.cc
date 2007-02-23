5f64751fc73645577135ab7b916506bcad1a2f85
pvozenilek <pvozenilek@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-02-23 09:54:39 +0000

    tests moved
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@801 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- modules/sybase/sybase.cc
+++ modules/sybase/sybase.cc
@@ -78,11 +78,6 @@ QoreNode* runSybaseTests(QoreNode* params, ExceptionSink* xsink)
     res.failed_test_file, res.failed_test_line);
   return 0;
 }
-
-TEST()
-{
-  // just an example of empty test
-}
 #endif
 
 static int sybase_commit(Datasource *ds, ExceptionSink *xsink);
@@ -209,19 +204,6 @@ void sybase_connection::init(char* username, char* password, char* dbname, Excep
     return;
   }
 }
-
-//------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-}
-#endif
 
 //------------------------------------------------------------------------------
 CS_RETCODE sybase_connection::message_callback()
@@ -387,83 +369,6 @@ void SybaseBindGroup::parseQuery(List* args, ExceptionSink* xsink)
 }
 
 //------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  ExceptionSink xsink;
-  QoreString str("select cound(*) from a_table where num = %v and value = %v");
-  SybaseBindGroup grp(&str);  
-  
-  List* lst = new List;
-  lst->push(new QoreNode("aaa"));
-  lst->push(new QoreNode("bbb"));
-
-  grp.parseQuery(lst, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  unsigned size = grp.m_input_parameters.size();
-  assert(size == 2);
-
-  char* new_str = grp.m_cmd->getBuffer();
-  if (strcmp(new_str, "select cound(*) from a_table where num =  ? and value =  ?") != 0) {
-    assert(false); // it needs to be formatted acc to Sybase 
-  }
-
-  QoreNode* n = new QoreNode(lst);
-  n->deref(&xsink);
-}
-
-TEST()
-{
-  // test with more arguments than placeholders (perhaps this should fail too)
-  ExceptionSink xsink;
-  QoreString str("select count(*) from a_table where num = %v and value = %v");
-  SybaseBindGroup grp(&str);
-
-  List* lst = new List;
-  lst->push(new QoreNode("aaa"));
-  lst->push(new QoreNode("bbb"));
-  lst->push(new QoreNode("ccc"));
-
-  grp.parseQuery(lst, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  unsigned size = grp.m_input_parameters.size();
-  assert(size == 2);
-
-  char* new_str = grp.m_cmd->getBuffer();
-  if (strcmp(new_str, "select count(*) from a_table where num =  ? and value =  ?") != 0) {
-    assert(false); // it needs to be formatted acc to Sybase
-  }
-
-  QoreNode* n = new QoreNode(lst);
-  n->deref(&xsink);
-}
-
-TEST()
-{
-  ExceptionSink xsink;
-  QoreString str("select count(*) from a_table where num = %v and value = %v");
-  SybaseBindGroup grp(&str);
-
-  List* lst = new List;
-  lst->push(new QoreNode("aaa"));
-
-  grp.parseQuery(lst, &xsink);
-  if (!xsink.isException()) { // not enought arguments
-    assert(false);
-  }
-  xsink.clear();
-
-  QoreNode* n = new QoreNode(lst);
-  n->deref(&xsink);
-}
-
-#endif
-
-//------------------------------------------------------------------------------
 CS_COMMAND* SybaseBindGroup::prepare_command(ExceptionSink* xsink)
 {
   CS_COMMAND* result = 0;
@@ -515,28 +420,6 @@ printf("### err 5: cmd = %s\n", m_cmd->getBuffer());
   g.Dismiss();
   return result;
 }
-
-//------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreString str("select count(*) from syskeys");
-  SybaseBindGroup grp(&str);
-  grp.m_connection = conn.getConnection();
-
-  grp.prepare_command(&xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-}
-#endif
 
 //------------------------------------------------------------------------------
 void SybaseBindGroup::deallocate_prepared_statement(CS_COMMAND* cmd, char* id)
@@ -1147,164 +1030,6 @@ printf("### err B\n");
 }
 
 //------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  // test used during the development
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreString str("select count(*) from syskeys where id > %v and id < %v");
-  SybaseBindGroup grp(&str);
-  grp.m_connection = conn.getConnection();
-
-  List* lst = new List;
-  lst->push(new QoreNode((int64)0));
-  lst->push(new QoreNode((int64)1000));
-
-  grp.parseQuery(lst, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  CS_COMMAND* cmd = grp.prepare_command(&xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  std::vector<SybaseBindGroup::column_info_t> inputs = grp.extract_input_parameters_info(cmd, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(inputs.size() == 2);
-
-  std::vector<SybaseBindGroup::column_info_t> outputs = grp.extract_output_parameters_info(cmd, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(outputs.size() == 1);
-
-  grp.bind_input_parameters(cmd, inputs, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreNode* res = grp.read_output(cmd, outputs, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(res);
-  // row "column1" with value 48 (on test machine)
-  if (res->type != NT_HASH) {
-    assert(false);
-  }
-  QoreNode* n = res->val.hash->getKeyValue("column1");
-  assert(n);
-  assert(n->type == NT_INT);
-  assert(n->val.intval == 48);
-  
-  res->deref(&xsink);
-  
-  QoreNode* aux = new QoreNode(lst);
-  aux->deref(&xsink);
-}
-
-TEST()
-{
-  // as above but using the exec_command()
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreString str("select count(*) from syskeys where id > %v and id < %v");
-  SybaseBindGroup grp(&str);
-  grp.m_connection = conn.getConnection();
-
-  List* lst = new List;
-  lst->push(new QoreNode((int64)0));
-  lst->push(new QoreNode((int64)1000));
-
-  grp.parseQuery(lst, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  QoreNode* n = grp.execute_command(&xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  n->deref(&xsink);
-}
-
-TEST()
-{
-  // select all columns
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreString str("select * from syskeys where id > %v and id < %v");
-  SybaseBindGroup grp(&str);
-  grp.m_connection = conn.getConnection();
-
-  List* lst = new List;
-  lst->push(new QoreNode((int64)0));
-  lst->push(new QoreNode((int64)1000));
-
-  grp.parseQuery(lst, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  CS_COMMAND* cmd = grp.prepare_command(&xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  std::vector<SybaseBindGroup::column_info_t> inputs = grp.extract_input_parameters_info(cmd, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(inputs.size() == 2);
-  std::vector<SybaseBindGroup::column_info_t> outputs = grp.extract_output_parameters_info(cmd, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(outputs.size() == 22); // on my machine
-
-  grp.bind_input_parameters(cmd, inputs, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-
-  QoreNode* res = grp.read_output(cmd, outputs, &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  assert(res);
-  if (res->type != NT_LIST) {
-    assert(false);
-  }
-  if (res->val.list->size() != 48) { // 48 on my machine
-    assert(false);
-  }
-
-  res->deref(&xsink);
-
-  QoreNode* aux = new QoreNode(lst);
-  aux->deref(&xsink);
-}
-
-#endif
-
-//------------------------------------------------------------------------------
 QoreNode* SybaseBindGroup::exec(class ExceptionSink *xsink)
 {
   QoreNode* n = execute_command(xsink);
@@ -1488,24 +1213,6 @@ static int sybase_commit(Datasource *ds, ExceptionSink *xsink)
 }
 
 //------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  // test sybase_commit()
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  int res = sybase_commit_impl(&conn, &xsink);
-  if (res != 1) {
-    assert(false);
-  }
-}
-#endif
-
-//------------------------------------------------------------------------------
 // Locally debugeable function
 static int sybase_rollback_impl(sybase_connection* sc, ExceptionSink* xsink)
 {
@@ -1551,24 +1258,6 @@ static int sybase_rollback(Datasource *ds, ExceptionSink *xsink)
   sybase_connection* sc = (sybase_connection*)ds->getPrivateData();
   return sybase_rollback_impl(sc, xsink);
 }
-
-//------------------------------------------------------------------------------
-#ifdef DEBUG
-TEST()
-{
-  // test sybase_rollback()
-  sybase_connection conn;
-  ExceptionSink xsink;
-  conn.init("sa", 0, "pavel", &xsink);
-  if (xsink.isException()) {
-    assert(false);
-  }
-  int res = sybase_rollback_impl(&conn, &xsink);
-  if (res != 1) {
-    assert(false);
-  }
-}
-#endif
 
 //------------------------------------------------------------------------------
 QoreString* sybase_module_init()
--- modules/sybase/tests/sybase_tests.cc
+++ modules/sybase/tests/sybase_tests.cc
@@ -1,7 +1,311 @@
 #ifdef DEBUG
 namespace sybase_tests_962876 {
 
+//------------------------------------------------------------------------------
+TEST()
+{
+  // basic test if we can connect
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // test whether parseQuery() works
+  ExceptionSink xsink;
+  QoreString str("select cound(*) from a_table where num = %v and value = %v");
+  SybaseBindGroup grp(&str);
+
+  List* lst = new List;
+  lst->push(new QoreNode("aaa"));
+  lst->push(new QoreNode("bbb"));
+
+  grp.parseQuery(lst, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  unsigned size = grp.m_input_parameters.size();
+  assert(size == 2);
+
+  char* new_str = grp.m_cmd->getBuffer();
+  if (strcmp(new_str, "select cound(*) from a_table where num =  ? and value =  ?") != 0) {
+    assert(false); // it needs to be formatted acc to Sybase
+  }
+
+  QoreNode* n = new QoreNode(lst);
+  n->deref(&xsink);
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // parseQuery() test with more arguments than placeholders 
+  // (perhaps this should fail too but this is how Oracle module behaves)
+  ExceptionSink xsink;
+  QoreString str("select count(*) from a_table where num = %v and value = %v");
+  SybaseBindGroup grp(&str);
+
+  List* lst = new List;
+  lst->push(new QoreNode("aaa"));
+  lst->push(new QoreNode("bbb"));
+  lst->push(new QoreNode("ccc"));
+
+  grp.parseQuery(lst, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  unsigned size = grp.m_input_parameters.size();
+  assert(size == 2);
+
+  char* new_str = grp.m_cmd->getBuffer();
+  if (strcmp(new_str, "select count(*) from a_table where num =  ? and value =  ?") != 0) {
+    assert(false); // it needs to be formatted acc to Sybase
+  }
+
+  QoreNode* n = new QoreNode(lst);
+  n->deref(&xsink);
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // parseQuery() test that fails
+  ExceptionSink xsink;
+  QoreString str("select count(*) from a_table where num = %v and value = %v");
+  SybaseBindGroup grp(&str);
+
+  List* lst = new List;
+  lst->push(new QoreNode("aaa"));
+
+  grp.parseQuery(lst, &xsink);
+  if (!xsink.isException()) { // not enought arguments
+    assert(false);
+  }
+  xsink.clear();
+
+  QoreNode* n = new QoreNode(lst);
+  n->deref(&xsink);
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // test sybase_commit()
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  int res = sybase_commit_impl(&conn, &xsink);
+  if (res != 1) {
+    assert(false);
+  }
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // test sybase_rollback()
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  int res = sybase_rollback_impl(&conn, &xsink);
+  if (res != 1) {
+    assert(false);
+  }
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // test for prepare_command()
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreString str("select count(*) from syskeys");
+  SybaseBindGroup grp(&str);
+  grp.m_connection = conn.getConnection();
+
+  grp.prepare_command(&xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // test used during the development
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreString str("select count(*) from syskeys where id > %v and id < %v");
+  SybaseBindGroup grp(&str);
+  grp.m_connection = conn.getConnection();
+
+  List* lst = new List;
+  lst->push(new QoreNode((int64)0));
+  lst->push(new QoreNode((int64)1000));
+
+  grp.parseQuery(lst, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  CS_COMMAND* cmd = grp.prepare_command(&xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  std::vector<SybaseBindGroup::column_info_t> inputs = grp.extract_input_parameters_info(cmd, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(inputs.size() == 2);
+
+  std::vector<SybaseBindGroup::column_info_t> outputs = grp.extract_output_parameters_info(cmd, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(outputs.size() == 1);
+
+  grp.bind_input_parameters(cmd, inputs, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreNode* res = grp.read_output(cmd, outputs, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(res);
+  // row "column1" with value 48 (on test machine)
+  if (res->type != NT_HASH) {
+    assert(false);
+  }
+  QoreNode* n = res->val.hash->getKeyValue("column1");
+  assert(n);
+  assert(n->type == NT_INT);
+  assert(n->val.intval == 48);
+
+  res->deref(&xsink);
+
+  QoreNode* aux = new QoreNode(lst);
+  aux->deref(&xsink);
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{/*
+  // as above but using the execute_command()
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreString str("select count(*) from syskeys where id > %v and id < %v");
+  SybaseBindGroup grp(&str);
+  grp.m_connection = conn.getConnection();
+
+  List* lst = new List;
+  lst->push(new QoreNode((int64)0));
+  lst->push(new QoreNode((int64)1000));
+
+  grp.parseQuery(lst, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  QoreNode* n = grp.execute_command(&xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  // TBD - check n
+  n->deref(&xsink);
+*/
+}
+
+//------------------------------------------------------------------------------
+TEST()
+{
+  // select all columns
+  sybase_connection conn;
+  ExceptionSink xsink;
+  conn.init("sa", 0, "pavel", &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreString str("select * from syskeys where id > %v and id < %v");
+  SybaseBindGroup grp(&str);
+  grp.m_connection = conn.getConnection();
+
+  List* lst = new List;
+  lst->push(new QoreNode((int64)0));
+  lst->push(new QoreNode((int64)1000));
+
+  grp.parseQuery(lst, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  CS_COMMAND* cmd = grp.prepare_command(&xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  std::vector<SybaseBindGroup::column_info_t> inputs = grp.extract_input_parameters_info(cmd, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(inputs.size() == 2);
+  std::vector<SybaseBindGroup::column_info_t> outputs = grp.extract_output_parameters_info(cmd, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(outputs.size() == 22); // on my machine
+
+  grp.bind_input_parameters(cmd, inputs, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+
+  QoreNode* res = grp.read_output(cmd, outputs, &xsink);
+  if (xsink.isException()) {
+    assert(false);
+  }
+  assert(res);
+  if (res->type != NT_LIST) {
+    assert(false);
+  }
+  if (res->val.list->size() != 48) { // 48 on my machine
+    assert(false);
+  }
+
+  res->deref(&xsink);
+
+  QoreNode* aux = new QoreNode(lst);
+  aux->deref(&xsink);
+}
+
 } // namespace
 #endif
+
 // EOF
 
