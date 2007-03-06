959eb840063ac0ad515745ff9c8f23b63ae94f4b
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-03-06 13:48:55 +0000

    added latin* aliases
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@841 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- lib/charset.cc
+++ lib/charset.cc
@@ -63,6 +63,41 @@ struct QoreEncoding *QoreEncodingManager::add(char *code, mbcs_length_t l, mbcs_
    return qcs;
 }
 
+// have to handle HP-UX' non-standard names for character sets separately
+#ifdef HPUX
+#define ISO88591_STR "ISO8859-1"
+#define ISO88592_STR "ISO8859-2"
+#define ISO88593_STR "ISO8859-3"
+#define ISO88594_STR "ISO8859-4"
+#define ISO88595_STR "ISO8859-5"
+#define ISO88596_STR "ISO8859-6"
+#define ISO88597_STR "ISO8859-7"
+#define ISO88598_STR "ISO8859-8"
+#define ISO88599_STR "ISO8859-9"
+#define ISO885910_STR "ISO8859-10"
+#define ISO885911_STR "ISO8859-11"
+#define ISO885913_STR "ISO8859-13"
+#define ISO885914_STR "ISO8859-14"
+#define ISO885915_STR "ISO8859-15"
+#define ISO885916_STR "ISO8859-16"
+#else
+#define ISO88591_STR "ISO-8859-1"
+#define ISO88592_STR "ISO-8859-2"
+#define ISO88593_STR "ISO-8859-3"
+#define ISO88594_STR "ISO-8859-4"
+#define ISO88595_STR "ISO-8859-5"
+#define ISO88596_STR "ISO-8859-6"
+#define ISO88597_STR "ISO-8859-7"
+#define ISO88598_STR "ISO-8859-8"
+#define ISO88599_STR "ISO-8859-9"
+#define ISO885910_STR "ISO-8859-10"
+#define ISO885911_STR "ISO-8859-11"
+#define ISO885913_STR "ISO-8859-13"
+#define ISO885914_STR "ISO-8859-14"
+#define ISO885915_STR "ISO-8859-15"
+#define ISO885916_STR "ISO-8859-16"
+#endif
+
 QoreEncodingManager::QoreEncodingManager()
 {
    // add character sets and setup aliases
@@ -75,221 +110,146 @@ QoreEncodingManager::QoreEncodingManager()
    QCS_UTF8        = addUnlocked("UTF-8",       utf8cl, utf8end, utf8cpos, "variable-width universal character set");
    addAlias(QCS_UTF8, "UTF8");
 
-#ifdef HPUX
-   // have to handle HP-UX' non-standard names for character sets separately
-   // not all of these are present in HP-UX, but we include them anyway
-   QCS_ISO_8859_1  = addUnlocked("ISO8859-1",  NULL,   NULL,    NULL,     "latin-1, Western European character set");
+   QCS_ISO_8859_1  = addUnlocked(ISO88591_STR,  NULL,   NULL,    NULL,     "latin-1, Western European character set");
    addAlias(QCS_ISO_8859_1, "ISO88591");
    addAlias(QCS_ISO_8859_1, "ISO-8859-1");
+   addAlias(QCS_ISO_8859_1, "ISO8859-1");
    addAlias(QCS_ISO_8859_1, "ISO-88591");
    addAlias(QCS_ISO_8859_1, "ISO8859P1");
    addAlias(QCS_ISO_8859_1, "ISO81");
+   addAlias(QCS_ISO_8859_1, "LATIN1");
+   addAlias(QCS_ISO_8859_1, "LATIN-1");
 
-   QCS_ISO_8859_2  = addUnlocked("ISO8859-2",  NULL,   NULL,    NULL,     "latin-2, Central European character set");
+   QCS_ISO_8859_2  = addUnlocked(ISO88592_STR,  NULL,   NULL,    NULL,     "latin-2, Central European character set");
    addAlias(QCS_ISO_8859_2, "ISO88592");
    addAlias(QCS_ISO_8859_2, "ISO-8859-2");
+   addAlias(QCS_ISO_8859_2, "ISO8859-2");
    addAlias(QCS_ISO_8859_2, "ISO-88592");
    addAlias(QCS_ISO_8859_2, "ISO8859P2");
    addAlias(QCS_ISO_8859_2, "ISO82");
+   addAlias(QCS_ISO_8859_2, "LATIN2");
+   addAlias(QCS_ISO_8859_2, "LATIN-2");
 
-   QCS_ISO_8859_3  = addUnlocked("ISO8859-3",  NULL,   NULL,    NULL,     "latin-3, Southern European character set");
+   QCS_ISO_8859_3  = addUnlocked(ISO88593_STR,  NULL,   NULL,    NULL,     "latin-3, Southern European character set");
    addAlias(QCS_ISO_8859_3, "ISO88593");
    addAlias(QCS_ISO_8859_3, "ISO-8859-3");
+   addAlias(QCS_ISO_8859_3, "ISO8859-3");
    addAlias(QCS_ISO_8859_3, "ISO-88593");
    addAlias(QCS_ISO_8859_3, "ISO8859P3");
    addAlias(QCS_ISO_8859_3, "ISO83");
+   addAlias(QCS_ISO_8859_3, "LATIN3");
+   addAlias(QCS_ISO_8859_3, "LATIN-3");
 
-   QCS_ISO_8859_4  = addUnlocked("ISO8859-4",  NULL,   NULL,    NULL,     "latin-4, Northern European character set");
+   QCS_ISO_8859_4  = addUnlocked(ISO88594_STR,  NULL,   NULL,    NULL,     "latin-4, Northern European character set");
    addAlias(QCS_ISO_8859_4, "ISO88594");
    addAlias(QCS_ISO_8859_4, "ISO-8859-4");
+   addAlias(QCS_ISO_8859_4, "ISO8859-4");
    addAlias(QCS_ISO_8859_4, "ISO-88594");
    addAlias(QCS_ISO_8859_4, "ISO8859P4");
    addAlias(QCS_ISO_8859_4, "ISO84");
+   addAlias(QCS_ISO_8859_4, "LATIN4");
+   addAlias(QCS_ISO_8859_4, "LATIN-4");
 
-   QCS_ISO_8859_5  = addUnlocked("ISO8859-5",  NULL,   NULL,    NULL,     "Cyrillic character set");
+   QCS_ISO_8859_5  = addUnlocked(ISO88595_STR,  NULL,   NULL,    NULL,     "Cyrillic character set");
    addAlias(QCS_ISO_8859_5, "ISO88595");
    addAlias(QCS_ISO_8859_5, "ISO-8859-5");
+   addAlias(QCS_ISO_8859_5, "ISO8859-5");
    addAlias(QCS_ISO_8859_5, "ISO-88595");
    addAlias(QCS_ISO_8859_5, "ISO8859P5");
    addAlias(QCS_ISO_8859_5, "ISO85");
 
-   QCS_ISO_8859_6  = addUnlocked("ISO8859-6",  NULL,   NULL,    NULL,     "Arabic character set");
+   QCS_ISO_8859_6  = addUnlocked(ISO88596_STR,  NULL,   NULL,    NULL,     "Arabic character set");
    addAlias(QCS_ISO_8859_6, "ISO88596");
    addAlias(QCS_ISO_8859_6, "ISO-8859-6");
+   addAlias(QCS_ISO_8859_6, "ISO8859-6");
    addAlias(QCS_ISO_8859_6, "ISO-88596");
    addAlias(QCS_ISO_8859_6, "ISO8859P6");
    addAlias(QCS_ISO_8859_6, "ISO86");
 
-   QCS_ISO_8859_7  = addUnlocked("ISO8859-7",  NULL,   NULL,    NULL,     "Greek character set");
+   QCS_ISO_8859_7  = addUnlocked(ISO88597_STR,  NULL,   NULL,    NULL,     "Greek character set");
    addAlias(QCS_ISO_8859_7, "ISO88597");
    addAlias(QCS_ISO_8859_7, "ISO-8859-7");
+   addAlias(QCS_ISO_8859_7, "ISO8859-7");
    addAlias(QCS_ISO_8859_7, "ISO-88597");
    addAlias(QCS_ISO_8859_7, "ISO8859P7");
    addAlias(QCS_ISO_8859_7, "ISO87");
 
-   QCS_ISO_8859_8  = addUnlocked("ISO8859-8",  NULL,   NULL,    NULL,     "Hebrew character set");
+   QCS_ISO_8859_8  = addUnlocked(ISO88598_STR,  NULL,   NULL,    NULL,     "Hebrew character set");
    addAlias(QCS_ISO_8859_8, "ISO88598");
    addAlias(QCS_ISO_8859_8, "ISO-8859-8");
+   addAlias(QCS_ISO_8859_8, "ISO8859-8");
    addAlias(QCS_ISO_8859_8, "ISO-88598");
    addAlias(QCS_ISO_8859_8, "ISO8859P8");
    addAlias(QCS_ISO_8859_8, "ISO88");
 
-   QCS_ISO_8859_9  = addUnlocked("ISO8859-9",  NULL,   NULL,    NULL,     "latin-5, Turkish character set");
+   QCS_ISO_8859_9  = addUnlocked(ISO88599_STR,  NULL,   NULL,    NULL,     "latin-5, Turkish character set");
    addAlias(QCS_ISO_8859_9, "ISO88599");
    addAlias(QCS_ISO_8859_9, "ISO-8859-9");
+   addAlias(QCS_ISO_8859_9, "ISO8859-9");
    addAlias(QCS_ISO_8859_9, "ISO-88599");
    addAlias(QCS_ISO_8859_9, "ISO8859P9");
    addAlias(QCS_ISO_8859_9, "ISO89");
+   addAlias(QCS_ISO_8859_9, "LATIN5");
+   addAlias(QCS_ISO_8859_9, "LATIN-5");
 
-   QCS_ISO_8859_10 = addUnlocked("ISO8859-10", NULL,   NULL,    NULL,     "latin-6, Nordic character set");
+   QCS_ISO_8859_10 = addUnlocked(ISO885910_STR, NULL,   NULL,    NULL,     "latin-6, Nordic character set");
    addAlias(QCS_ISO_8859_10, "ISO885910");
    addAlias(QCS_ISO_8859_10, "ISO-8859-10");
+   addAlias(QCS_ISO_8859_10, "ISO8859-10");
    addAlias(QCS_ISO_8859_10, "ISO-885910");
    addAlias(QCS_ISO_8859_10, "ISO8859P10");
    addAlias(QCS_ISO_8859_10, "ISO810");
+   addAlias(QCS_ISO_8859_10, "LATIN6");
+   addAlias(QCS_ISO_8859_10, "LATIN-6");
 
-   QCS_ISO_8859_11 = addUnlocked("ISO8859-11", NULL,   NULL,    NULL,     "Thai character set");
+   QCS_ISO_8859_11 = addUnlocked(ISO885911_STR, NULL,   NULL,    NULL,     "Thai character set");
    addAlias(QCS_ISO_8859_11, "ISO885911");
    addAlias(QCS_ISO_8859_11, "ISO-8859-11");
+   addAlias(QCS_ISO_8859_11, "ISO8859-11");
    addAlias(QCS_ISO_8859_11, "ISO-885911");
    addAlias(QCS_ISO_8859_11, "ISO8859P11");
    addAlias(QCS_ISO_8859_11, "ISO811");
 
    // there is no ISO-8859-12
-   QCS_ISO_8859_13 = addUnlocked("ISO8859-13", NULL,   NULL,    NULL,     "Latin-7, Baltic rim character set");
+   QCS_ISO_8859_13 = addUnlocked(ISO885913_STR, NULL,   NULL,    NULL,     "Latin-7, Baltic rim character set");
    addAlias(QCS_ISO_8859_13, "ISO885913");
    addAlias(QCS_ISO_8859_13, "ISO-8859-13");
+   addAlias(QCS_ISO_8859_13, "ISO8859-13");
    addAlias(QCS_ISO_8859_13, "ISO-885913");
    addAlias(QCS_ISO_8859_13, "ISO8859P13");
    addAlias(QCS_ISO_8859_13, "ISO813");
+   addAlias(QCS_ISO_8859_13, "LATIN7");
+   addAlias(QCS_ISO_8859_13, "LATIN-7");
 
-   QCS_ISO_8859_14 = addUnlocked("ISO8859-14", NULL,   NULL,    NULL,     "Latin-8, Celtic character set");
+   QCS_ISO_8859_14 = addUnlocked(ISO885914_STR, NULL,   NULL,    NULL,     "Latin-8, Celtic character set");
    addAlias(QCS_ISO_8859_14, "ISO885914");
    addAlias(QCS_ISO_8859_14, "ISO-8859-14");
+   addAlias(QCS_ISO_8859_14, "ISO8859-14");
    addAlias(QCS_ISO_8859_14, "ISO-885914");
    addAlias(QCS_ISO_8859_14, "ISO8859P14");
    addAlias(QCS_ISO_8859_14, "ISO814");
+   addAlias(QCS_ISO_8859_14, "LATIN8");
+   addAlias(QCS_ISO_8859_14, "LATIN-8");
 
-   QCS_ISO_8859_15 = addUnlocked("ISO8859-15", NULL,   NULL,    NULL,     "Latin-9, Western European with euro symbol");
+   QCS_ISO_8859_15 = addUnlocked(ISO885915_STR, NULL,   NULL,    NULL,     "Latin-9, Western European with euro symbol");
    addAlias(QCS_ISO_8859_15, "ISO885915");
    addAlias(QCS_ISO_8859_15, "ISO-8859-15");
+   addAlias(QCS_ISO_8859_15, "ISO8859-15");
    addAlias(QCS_ISO_8859_15, "ISO-885915");
    addAlias(QCS_ISO_8859_15, "ISO8859P15");
    addAlias(QCS_ISO_8859_15, "ISO815");
+   addAlias(QCS_ISO_8859_15, "LATIN9");
+   addAlias(QCS_ISO_8859_15, "LATIN-9");
 
-   QCS_ISO_8859_16 = addUnlocked("ISO8859-16", NULL,   NULL,    NULL,     "Latin-10, Southeast European character set");
+   QCS_ISO_8859_16 = addUnlocked(ISO885916_STR, NULL,   NULL,    NULL,     "Latin-10, Southeast European character set");
    addAlias(QCS_ISO_8859_16, "ISO885916");
    addAlias(QCS_ISO_8859_16, "ISO-8859-16");
+   addAlias(QCS_ISO_8859_16, "ISO8859-16");
    addAlias(QCS_ISO_8859_16, "ISO-885916");
    addAlias(QCS_ISO_8859_16, "ISO8859P16");
    addAlias(QCS_ISO_8859_16, "ISO816");
-#else
-   QCS_ISO_8859_1  = addUnlocked("ISO8859-1",  NULL,   NULL,    NULL,     "latin-1, Western European character set");
-   addAlias(QCS_ISO_8859_1, "ISO88591");
-   addAlias(QCS_ISO_8859_1, "ISO-8859-1");
-   addAlias(QCS_ISO_8859_1, "ISO-88591");
-   addAlias(QCS_ISO_8859_1, "ISO8859P1");
-   addAlias(QCS_ISO_8859_1, "ISO81");
-
-   QCS_ISO_8859_2  = addUnlocked("ISO8859-2",  NULL,   NULL,    NULL,     "latin-2, Central European character set");
-   addAlias(QCS_ISO_8859_2, "ISO88592");
-   addAlias(QCS_ISO_8859_2, "ISO-8859-2");
-   addAlias(QCS_ISO_8859_2, "ISO-88592");
-   addAlias(QCS_ISO_8859_2, "ISO8859P2");
-   addAlias(QCS_ISO_8859_2, "ISO82");
-
-   QCS_ISO_8859_3  = addUnlocked("ISO8859-3",  NULL,   NULL,    NULL,     "latin-3, Southern European character set");
-   addAlias(QCS_ISO_8859_3, "ISO88593");
-   addAlias(QCS_ISO_8859_3, "ISO-8859-3");
-   addAlias(QCS_ISO_8859_3, "ISO-88593");
-   addAlias(QCS_ISO_8859_3, "ISO8859P3");
-   addAlias(QCS_ISO_8859_3, "ISO83");
-
-   QCS_ISO_8859_4  = addUnlocked("ISO8859-4",  NULL,   NULL,    NULL,     "latin-4, Northern European character set");
-   addAlias(QCS_ISO_8859_4, "ISO88594");
-   addAlias(QCS_ISO_8859_4, "ISO-8859-4");
-   addAlias(QCS_ISO_8859_4, "ISO-88594");
-   addAlias(QCS_ISO_8859_4, "ISO8859P4");
-   addAlias(QCS_ISO_8859_4, "ISO84");
-
-   QCS_ISO_8859_5  = addUnlocked("ISO8859-5",  NULL,   NULL,    NULL,     "Cyrillic character set");
-   addAlias(QCS_ISO_8859_5, "ISO88595");
-   addAlias(QCS_ISO_8859_5, "ISO-8859-5");
-   addAlias(QCS_ISO_8859_5, "ISO-88595");
-   addAlias(QCS_ISO_8859_5, "ISO8859P5");
-   addAlias(QCS_ISO_8859_5, "ISO85");
-
-   QCS_ISO_8859_6  = addUnlocked("ISO8859-6",  NULL,   NULL,    NULL,     "Arabic character set");
-   addAlias(QCS_ISO_8859_6, "ISO88596");
-   addAlias(QCS_ISO_8859_6, "ISO-8859-6");
-   addAlias(QCS_ISO_8859_6, "ISO-88596");
-   addAlias(QCS_ISO_8859_6, "ISO8859P6");
-   addAlias(QCS_ISO_8859_6, "ISO86");
-
-   QCS_ISO_8859_7  = addUnlocked("ISO8859-7",  NULL,   NULL,    NULL,     "Greek character set");
-   addAlias(QCS_ISO_8859_7, "ISO88597");
-   addAlias(QCS_ISO_8859_7, "ISO-8859-7");
-   addAlias(QCS_ISO_8859_7, "ISO-88597");
-   addAlias(QCS_ISO_8859_7, "ISO8859P7");
-   addAlias(QCS_ISO_8859_7, "ISO87");
-
-   QCS_ISO_8859_8  = addUnlocked("ISO8859-8",  NULL,   NULL,    NULL,     "Hebrew character set");
-   addAlias(QCS_ISO_8859_8, "ISO88598");
-   addAlias(QCS_ISO_8859_8, "ISO-8859-8");
-   addAlias(QCS_ISO_8859_8, "ISO-88598");
-   addAlias(QCS_ISO_8859_8, "ISO8859P8");
-   addAlias(QCS_ISO_8859_8, "ISO88");
-
-   QCS_ISO_8859_9  = addUnlocked("ISO8859-9",  NULL,   NULL,    NULL,     "latin-5, Turkish character set");
-   addAlias(QCS_ISO_8859_9, "ISO88599");
-   addAlias(QCS_ISO_8859_9, "ISO-8859-9");
-   addAlias(QCS_ISO_8859_9, "ISO-88599");
-   addAlias(QCS_ISO_8859_9, "ISO8859P9");
-   addAlias(QCS_ISO_8859_9, "ISO89");
-
-   QCS_ISO_8859_10 = addUnlocked("ISO8859-10", NULL,   NULL,    NULL,     "latin-6, Nordic character set");
-   addAlias(QCS_ISO_8859_10, "ISO885910");
-   addAlias(QCS_ISO_8859_10, "ISO-8859-10");
-   addAlias(QCS_ISO_8859_10, "ISO-885910");
-   addAlias(QCS_ISO_8859_10, "ISO8859P10");
-   addAlias(QCS_ISO_8859_10, "ISO810");
-
-   QCS_ISO_8859_11 = addUnlocked("ISO8859-11", NULL,   NULL,    NULL,     "Thai character set");
-   addAlias(QCS_ISO_8859_11, "ISO885911");
-   addAlias(QCS_ISO_8859_11, "ISO-8859-11");
-   addAlias(QCS_ISO_8859_11, "ISO-885911");
-   addAlias(QCS_ISO_8859_11, "ISO8859P11");
-   addAlias(QCS_ISO_8859_11, "ISO811");
-
-   // there is no ISO-8859-12
-   QCS_ISO_8859_13 = addUnlocked("ISO8859-13", NULL,   NULL,    NULL,     "Latin-7, Baltic rim character set");
-   addAlias(QCS_ISO_8859_13, "ISO885913");
-   addAlias(QCS_ISO_8859_13, "ISO-8859-13");
-   addAlias(QCS_ISO_8859_13, "ISO-885913");
-   addAlias(QCS_ISO_8859_13, "ISO8859P13");
-   addAlias(QCS_ISO_8859_13, "ISO813");
-
-   QCS_ISO_8859_14 = addUnlocked("ISO8859-14", NULL,   NULL,    NULL,     "Latin-8, Celtic character set");
-   addAlias(QCS_ISO_8859_14, "ISO885914");
-   addAlias(QCS_ISO_8859_14, "ISO-8859-14");
-   addAlias(QCS_ISO_8859_14, "ISO-885914");
-   addAlias(QCS_ISO_8859_14, "ISO8859P14");
-   addAlias(QCS_ISO_8859_14, "ISO814");
-
-   QCS_ISO_8859_15 = addUnlocked("ISO8859-15", NULL,   NULL,    NULL,     "Latin-9, Western European with euro symbol");
-   addAlias(QCS_ISO_8859_15, "ISO885915");
-   addAlias(QCS_ISO_8859_15, "ISO-8859-15");
-   addAlias(QCS_ISO_8859_15, "ISO-885915");
-   addAlias(QCS_ISO_8859_15, "ISO8859P15");
-   addAlias(QCS_ISO_8859_15, "ISO815");
-
-   QCS_ISO_8859_16 = addUnlocked("ISO8859-16", NULL,   NULL,    NULL,     "Latin-10, Southeast European character set");
-   addAlias(QCS_ISO_8859_16, "ISO885916");
-   addAlias(QCS_ISO_8859_16, "ISO-8859-16");
-   addAlias(QCS_ISO_8859_16, "ISO-885916");
-   addAlias(QCS_ISO_8859_16, "ISO8859P16");
-   addAlias(QCS_ISO_8859_16, "ISO816");
-#endif
+   addAlias(QCS_ISO_8859_16, "LATIN10");
+   addAlias(QCS_ISO_8859_16, "LATIN-10");
 
    QCS_KOI8_R      = addUnlocked("KOI8-R",      NULL,   NULL,    NULL,     "Russian: Kod Obmena Informatsiey, 8 bit");
    addAlias(QCS_KOI8_R, "KOI8R");
