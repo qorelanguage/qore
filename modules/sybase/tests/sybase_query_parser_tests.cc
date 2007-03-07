84705ef92a7e69c129d4a148e06e6ee9199f9b6a
pvozenilek <pvozenilek@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-03-07 00:05:30 +0000

    more Sybase code
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@854 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ modules/sybase/tests/sybase_query_parser_tests.cc
@@ -0,0 +1,15 @@
+#ifdef DEBUG
+
+#include "sybase_tests_common.h"
+
+namespace sybase_tests_1739 {
+
+//------------------------------------------------------------------------------
+
+} // namespace
+#endif
+
+// EOF
+
+
+
