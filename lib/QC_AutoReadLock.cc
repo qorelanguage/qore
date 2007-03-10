1a51b81a6595ec82ee8767f373d6ae78ceb0dbad
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-03-10 15:01:17 +0000

    missing files
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@894 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ lib/QC_AutoReadLock.cc
@@ -0,0 +1,71 @@
+/*
+ QC_AutoReadLock.cc
+ 
+ Qore Programming Language
+ 
+ Copyright (C) 2003, 2004, 2005, 2006, 2007 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <qore/Qore.h>
+#include <qore/QC_AutoReadLock.h>
+
+int CID_AUTOREADLOCK;
+
+static void ARL_constructor(class Object *self, class QoreNode *params, ExceptionSink *xsink)
+{
+   class QoreNode *p = test_param(params, NT_OBJECT, 0);
+   QoreRWLock *rwl = p ? (QoreRWLock *)p->val.object->getReferencedPrivateData(CID_RWLOCK, xsink) : NULL;
+   if (xsink->isException())
+      return;
+
+   if (!rwl)
+   {
+      xsink->raiseException("AUTOREADLOCK-CONSTRUCTOR-ERROR", "expecting RWLock type as argument to constructor");
+      return;
+   }
+
+   QoreAutoReadLock *arwl = new QoreAutoReadLock(rwl, xsink);
+   if (*xsink)
+      arwl->deref(xsink);
+   else
+      self->setPrivate(CID_AUTOREADLOCK, arwl);
+}
+
+static void ARL_destructor(class Object *self, class QoreAutoReadLock *arwl, ExceptionSink *xsink)
+{
+   arwl->destructor(xsink);
+   arwl->deref(xsink);
+}
+
+static void ARL_copy(class Object *self, class Object *old, class QoreAutoReadLock *m, ExceptionSink *xsink)
+{
+   xsink->raiseException("AUTOREADLOCK-COPY-ERROR", "objects of this class cannot be copied");
+}
+
+class QoreClass *initAutoReadLockClass()
+{
+   tracein("initAutoReadLockClass()");
+   
+   class QoreClass *QC_AutoReadLock = new QoreClass(QDOM_THREAD_CLASS, strdup("AutoReadLock"));
+   CID_AUTOREADLOCK = QC_AutoReadLock->getID();
+   QC_AutoReadLock->setConstructor(ARL_constructor);
+   QC_AutoReadLock->setDestructor((q_destructor_t)ARL_destructor);
+   QC_AutoReadLock->setCopy((q_copy_t)ARL_copy);
+   
+   traceout("initAutoReadLockClass()");
+   return QC_AutoReadLock;
+}
---
+++ lib/QC_AutoWriteLock.cc
@@ -0,0 +1,71 @@
+/*
+ QC_AutoWriteLock.cc
+ 
+ Qore Programming Language
+ 
+ Copyright (C) 2003, 2004, 2005, 2006, 2007 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <qore/Qore.h>
+#include <qore/QC_AutoWriteLock.h>
+
+int CID_AUTOWRITELOCK;
+
+static void AWL_constructor(class Object *self, class QoreNode *params, ExceptionSink *xsink)
+{
+   class QoreNode *p = test_param(params, NT_OBJECT, 0);
+   QoreRWLock *rwl = p ? (QoreRWLock *)p->val.object->getReferencedPrivateData(CID_RWLOCK, xsink) : NULL;
+   if (xsink->isException())
+      return;
+
+   if (!rwl)
+   {
+      xsink->raiseException("AUTOWRITELOCK-CONSTRUCTOR-ERROR", "expecting RWLock type as argument to constructor");
+      return;
+   }
+
+   QoreAutoWriteLock *arwl = new QoreAutoWriteLock(rwl, xsink);
+   if (*xsink)
+      arwl->deref(xsink);
+   else
+      self->setPrivate(CID_AUTOWRITELOCK, arwl);
+}
+
+static void AWL_destructor(class Object *self, class QoreAutoWriteLock *arwl, ExceptionSink *xsink)
+{
+   arwl->destructor(xsink);
+   arwl->deref(xsink);
+}
+
+static void AWL_copy(class Object *self, class Object *old, class QoreAutoWriteLock *m, ExceptionSink *xsink)
+{
+   xsink->raiseException("AUTOWRITELOCK-COPY-ERROR", "objects of this class cannot be copied");
+}
+
+class QoreClass *initAutoWriteLockClass()
+{
+   tracein("initAutoWriteLockClass()");
+   
+   class QoreClass *QC_AutoWriteLock = new QoreClass(QDOM_THREAD_CLASS, strdup("AutoWriteLock"));
+   CID_AUTOWRITELOCK = QC_AutoWriteLock->getID();
+   QC_AutoWriteLock->setConstructor(AWL_constructor);
+   QC_AutoWriteLock->setDestructor((q_destructor_t)AWL_destructor);
+   QC_AutoWriteLock->setCopy((q_copy_t)AWL_copy);
+   
+   traceout("initAutoWriteLockClass()");
+   return QC_AutoWriteLock;
+}
