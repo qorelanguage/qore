0cf1a4f7ccb0c4d1cbbf2535c51fca52759086f6
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-03-23 12:07:59 +0000

    removed commits in pgsql driver
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1000 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- modules/pgsql/pgsql.cc
+++ modules/pgsql/pgsql.cc
@@ -76,6 +76,8 @@ static class QoreNode *qore_pgsql_select_rows(class Datasource *ds, QoreString *
 {
    QorePGConnection *pc = (QorePGConnection *)ds->getPrivateData();
 
+   return pc->select_rows(ds, qstr, args, xsink);
+/*
    class QoreNode *rv = pc->select_rows(ds, qstr, args, xsink);
    if (!*xsink && ds->getAutoCommit() && pc->commit(ds, xsink))
    {
@@ -83,12 +85,15 @@ static class QoreNode *qore_pgsql_select_rows(class Datasource *ds, QoreString *
       return NULL;
    }
    return rv;
+*/
 }
 
 static class QoreNode *qore_pgsql_select(class Datasource *ds, QoreString *qstr, class List *args, class ExceptionSink *xsink)
 {
    QorePGConnection *pc = (QorePGConnection *)ds->getPrivateData();
 
+   return pc->select(ds, qstr, args, xsink);
+/*
    class QoreNode *rv = pc->select(ds, qstr, args, xsink);
    if (!*xsink && ds->getAutoCommit() && pc->commit(ds, xsink))
    {
@@ -96,12 +101,15 @@ static class QoreNode *qore_pgsql_select(class Datasource *ds, QoreString *qstr,
       return NULL;
    }
    return rv;
+*/
 }
 
 static class QoreNode *qore_pgsql_exec(class Datasource *ds, QoreString *qstr, class List *args, class ExceptionSink *xsink)
 {
    QorePGConnection *pc = (QorePGConnection *)ds->getPrivateData();
 
+   return pc->exec(ds, qstr, args, xsink);
+/*
    class QoreNode *rv = pc->exec(ds, qstr, args, xsink);
    if (!*xsink && ds->getAutoCommit() && pc->commit(ds, xsink))
    {
@@ -109,6 +117,7 @@ static class QoreNode *qore_pgsql_exec(class Datasource *ds, QoreString *qstr, c
       return NULL;
    }
    return rv;
+*/
 }
 
 static int qore_pgsql_open(Datasource *ds, ExceptionSink *xsink)
