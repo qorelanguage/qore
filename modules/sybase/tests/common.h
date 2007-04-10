a6879667e046936dcbfa5d0afdda0403ab23c6e4
pvozenilek <pvozenilek@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-04-10 13:26:18 +0000

    file renamed
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1052 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ modules/sybase/tests/common.h
@@ -0,0 +1,30 @@
+#ifndef SYBASE_TESTS_COMMON_H_
+#define SYBASE_TESTS_COMMON_H_
+
+// Common settings for all unit tests
+
+#ifdef SYBASE
+// Server running locally
+
+// Connection parameters used for tests
+// 1. user name
+// 2. password
+// 3. database name 
+//  
+#define SYBASE_TEST_SETTINGS "sa", 0, "pavel"
+
+#else
+// MS SQL Server running on VMWare host machine, details in freedts.conf
+//#define SYBASE_TEST_SETTINGS "sa", "pavel", "mssql"
+
+// local Sybase connected via FreeTDS, details in freedts.conf
+#define SYBASE_TEST_SETTINGS "sa", 0, "pavel"
+
+
+#endif
+
+#endif
+
+// EOF
+
+
