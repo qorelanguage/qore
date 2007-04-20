ca4f4f61358ab74db5cc9606759185d55095a5bd
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-04-20 08:07:09 +0000

    fixed typos in error strings
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1112 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- modules/mysql/mysql.cc
+++ modules/mysql/mysql.cc
@@ -556,13 +556,13 @@ inline int MyBindGroup::parse(class List *args, class ExceptionSink *xsink)
 	 }
 	 if ((*p) != 'v')
 	 {
-	    xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%d', got %%%c)", *p);
+	    xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%%d', got %%%c)", *p);
 	    return -1;
 	 }
 	 p++;
 	 if (isalpha(*p))
 	 {
-	    xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%d', got %%v%c*)", *p);
+	    xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%%d', got %%v%c*)", *p);
 	    return -1;
 	 }
 
--- modules/pgsql/QorePGConnection.cc
+++ modules/pgsql/QorePGConnection.cc
@@ -1301,13 +1301,13 @@ int QorePGResult::parse(class QoreString *str, class List *args, class Exception
 	 }
          if ((*p) != 'v')
          {
-            xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%d', got %%%c)", *p);
+            xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%%d', got %%%c)", *p);
             return -1;
          }
          p++;
          if (isalpha(*p))
          {
-            xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%d', got %%v%c*)", *p);
+            xsink->raiseException("DBI-EXEC-PARSE-EXCEPTION", "invalid value specification (expecting '%v' or '%%d', got %%v%c*)", *p);
             return -1;
          }
 
