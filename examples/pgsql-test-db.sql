ee5e44dbe825a94c331e2ce5005d6a0d783dba3e
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-05-22 20:32:20 +0000

    new file
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1226 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ examples/pgsql-test-db.sql
@@ -0,0 +1,44 @@
+
+create table family (
+   family_id int not null,
+   name varchar(80) not null
+);
+
+insert into family values ( 1, 'Smith' );
+insert into family values ( 2, 'Jones' );
+
+create table people (
+   person_id int not null,
+   family_id int not null,
+   name varchar(250) not null,
+   dob date not null
+);
+
+insert into people values ( 1, 1, 'Arnie', to_date('1983-05-13', 'YYYY-MM-DD') );
+insert into people values ( 2, 1, 'Sylvia', to_date('1994-11-10', 'YYYY-MM-DD') );
+insert into people values ( 3, 1, 'Carol', to_date('2003-07-23', 'YYYY-MM-DD') );
+insert into people values ( 4, 1, 'Bernard', to_date('1979-02-27', 'YYYY-MM-DD') );
+insert into people values ( 5, 1, 'Isaac', to_date('2000-04-04', 'YYYY-MM-DD') );
+insert into people values ( 6, 2, 'Alan', to_date('1992-06-04', 'YYYY-MM-DD') );
+insert into people values ( 7, 2, 'John', to_date('1995-03-23', 'YYYY-MM-DD') );
+
+create table attributes (
+   person_id int not null,
+   attribute varchar(80) not null,
+   value varchar(160) not null
+);
+
+insert into attributes values ( 1, 'hair', 'blond' );
+insert into attributes values ( 1, 'eyes', 'brown' );
+insert into attributes values ( 2, 'hair', 'blond' );
+insert into attributes values ( 2, 'eyes', 'blue');
+insert into attributes values ( 3, 'hair', 'brown' );
+insert into attributes values ( 3, 'eyes', 'green');
+insert into attributes values ( 4, 'hair', 'brown' );
+insert into attributes values ( 4, 'eyes', 'brown');
+insert into attributes values ( 5, 'hair', 'red' );
+insert into attributes values ( 5, 'eyes', 'green');
+insert into attributes values ( 6, 'hair', 'black' );
+insert into attributes values ( 6, 'eyes', 'blue');
+insert into attributes values ( 7, 'hair', 'brown' );
+insert into attributes values ( 7, 'eyes', 'brown');
