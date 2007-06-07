60ace09b1ff7d21c44a2c48b3c392e7b242feea1
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-06-07 11:47:10 +0000

    missing files
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1306 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ include/qore/ParseOptionMap.h
@@ -0,0 +1,43 @@
+/*
+ ParseOptionMap.h
+ 
+ Qore Programming language
+ 
+ Copyright (C) 2003, 2004, 2005, 2006, 2007 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _QORE_PARSEOPTIONMAP_H
+#define _QORE_PARSEOPTIONMAP_H
+
+typedef std::map<const char *, int, ltstr> opt_map_t;
+typedef std::map<int, const char *> rev_opt_map_t;
+
+class ParseOptionMap {
+   private:
+      DLLLOCAL static opt_map_t map;
+      DLLLOCAL static rev_opt_map_t rmap;
+
+   public:
+      DLLLOCAL ParseOptionMap();
+      DLLLOCAL static void static_init();
+      
+      DLLEXPORT static const char *find_name(int code);
+      DLLEXPORT static int find_code(const char *name);
+      DLLEXPORT static void list_options();
+};
+
+#endif
