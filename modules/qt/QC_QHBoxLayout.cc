0e211afe0aa3535de2c6b6eb665e5dfe8ac349b1
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2007-07-25 12:37:56 +0000

    missing files
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1360 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ modules/qt/QC_QHBoxLayout.cc
@@ -0,0 +1,67 @@
+/*
+ QC_QHBoxLayout.cc
+ 
+ Qore Programming Language
+ 
+ Copyright (C) 2003, 2004, 2005, 2006, 2007 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <qore/Qore.h>
+
+#include "QC_QHBoxLayout.h"
+#include "QC_QFont.h"
+#include "QC_QWidget.h"
+
+int CID_QHBOXLAYOUT;
+
+static void QHBOXL_constructor(class Object *self, class QoreNode *params, ExceptionSink *xsink)
+{
+   QoreQHBoxLayout *qw;
+   QoreNode *p = test_param(params, NT_OBJECT, 0);
+   QoreAbstractQWidget *parent = p ? (QoreAbstractQWidget *)p->val.object->getReferencedPrivateData(CID_QWIDGET, xsink) : 0;
+
+   if (!parent)
+      qw = new QoreQHBoxLayout();
+   else 
+   {
+      ReferenceHolder<QoreAbstractQWidget> holder(parent, xsink);
+      qw = new QoreQHBoxLayout(parent->getQWidget());
+   }
+
+   self->setPrivate(CID_QHBOXLAYOUT, qw);
+}
+
+static void QHBOXL_copy(class Object *self, class Object *old, class QoreQHBoxLayout *ql, ExceptionSink *xsink)
+{
+   xsink->raiseException("QHBOXLAYOUT-COPY-ERROR", "objects of this class cannot be copied");
+}
+
+class QoreClass *initQHBoxLayoutClass(class QoreClass *qboxlayout)
+{
+   tracein("initQHBoxLayoutClass()");
+   
+   class QoreClass *QC_QHBoxLayout = new QoreClass("QHBoxLayout", QDOM_GUI);
+   CID_QHBOXLAYOUT = QC_QHBoxLayout->getID();
+
+   QC_QHBoxLayout->addBuiltinVirtualBaseClass(qboxlayout);
+
+   QC_QHBoxLayout->setConstructor(QHBOXL_constructor);
+   QC_QHBoxLayout->setCopy((q_copy_t)QHBOXL_copy);
+
+   traceout("initQHBoxLayoutClass()");
+   return QC_QHBoxLayout;
+}
---
+++ modules/qt/QC_QHBoxLayout.h
@@ -0,0 +1,64 @@
+/*
+ QC_QHBoxLayout.h
+ 
+ Qore Programming Language
+ 
+ Copyright (C) 2003, 2004, 2005, 2006, 2007 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _QORE_QC_QHBOXLAYOUT_H
+
+#define _QORE_QC_QHBOXLAYOUT_H
+
+#include "QoreAbstractQBoxLayout.h"
+
+#include <QHBoxLayout>
+
+DLLEXPORT extern int CID_QHBOXLAYOUT;
+
+DLLLOCAL class QoreClass *initQHBoxLayoutClass();
+
+class QoreQHBoxLayout : public QoreAbstractQBoxLayout
+{
+   public:
+      QPointer<QHBoxLayout> qobj;
+
+      DLLLOCAL QoreQHBoxLayout() : qobj(new QHBoxLayout)
+      {
+      }
+
+      DLLLOCAL QoreQHBoxLayout(QWidget *parent) : qobj(new QHBoxLayout(parent))
+      {
+      }
+      DLLLOCAL virtual QObject *getQObject() const
+      {
+	 return static_cast<QObject *>(&(*qobj));
+      }
+
+      DLLLOCAL virtual QLayout *getQLayout() const
+      {
+	 return static_cast<QLayout *>(&(*qobj));
+      }
+
+      DLLLOCAL virtual QBoxLayout *getQBoxLayout() const
+      {
+	 return static_cast<QBoxLayout *>(&(*qobj));
+      }
+
+};
+
+#endif
