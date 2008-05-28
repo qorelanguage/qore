63e61441f00a49a2195cd19f7e1bb2e9c34dcd53
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2008-05-28 20:12:05 +0000

    cosmetic updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@1812 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- command-line.cc
+++ command-line.cc
@@ -289,11 +289,9 @@ static void do_version(char *arg)
    printf("QORE for %s %s (%d-bit build), Copyright (C) 2003 - 2007 David Nichols\nversion %s", qore_target_os, qore_target_arch, qore_target_bits, qore_version_string);
 
    FeatureList::iterator i = qoreFeatureList.begin();
-   if (i != qoreFeatureList.end())
-   {
+   if (i != qoreFeatureList.end()) {
       printf(" (builtin features: ");
-      while (i != qoreFeatureList.end())
-      {
+      while (i != qoreFeatureList.end()) {
 	 printf((*i).c_str());
 	 i++;
 	 if (i != qoreFeatureList.end())
