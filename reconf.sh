94e591d78d4f318b6040fa0eb07377a515b87c5e
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2009-01-07 11:53:14 +0000

    configure fixes
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@2372 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- reconf.sh
+++ reconf.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
-rm -f config.cache acconfig.h aclocal.m4
+rm -f config.cache acconfig.h aclocal.m4 config.guess config.sub ltmain.sh
 libtoolize
 cat m4/*.m4 > acinclude.m4
 aclocal -I m4
