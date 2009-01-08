ce7357d1826231fa9216aa84c8066805319eddbd
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2009-01-08 14:42:55 +0000

    updated reconf.sh to look for glibtoolize as well as libtoolize
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@2390 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- reconf.sh
+++ reconf.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+# see if we need libtoolize or glibtoolize
+which libtoolize >/dev/null
+if [ $? -eq 0 ]; then
+   lcmd=libtoolize
+fi
+which glibtoolize >/dev/null
+if [ $? -eq 0 ]; then
+   lcmd=glibtoolize
+fi
+if [ -z "$lcmd" ]; then
+    echo ERROR: please install libtoolize or glibtoolize before running this script
+fi
 set -x
 rm -f config.cache acconfig.h aclocal.m4 config.guess config.sub ltmain.sh
-libtoolize
+$lcmd
 cat m4/*.m4 > acinclude.m4
 aclocal -I m4
 autoconf
