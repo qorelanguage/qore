28502fb3fae16b206e5b22a969e05dbf5c505c71
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2010-03-12 13:26:51 +0000

    added missing files
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@3244 1b0b35fe-f740-4c1b-b404-a338b32e513b

---
+++ include/qore/intern/QoreExtractOperatorNode.h
@@ -0,0 +1,67 @@
+/* -*- mode: c++; indent-tabs-mode: nil -*- */
+/*
+ QoreExtractOperatorNode.h
+ 
+ Qore Programming Language
+ 
+ Copyright 2003 - 2010 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef _QORE_QOREEXTRACTOPERATORNODE_H
+
+#define _QORE_QOREEXTRACTOPERATORNODE_H
+
+class QoreExtractOperatorNode : public QoreOperatorNode {
+protected:
+   AbstractQoreNode *lvalue_exp, *offset_exp, *length_exp, *new_exp;
+
+   DLLLOCAL static QoreString extract_str;
+
+   DLLLOCAL virtual AbstractQoreNode *evalImpl(ExceptionSink *xsink) const;
+   DLLLOCAL virtual AbstractQoreNode *evalImpl(bool &needs_deref, ExceptionSink *xsink) const;
+
+   DLLLOCAL ~QoreExtractOperatorNode() {
+      discard(lvalue_exp, 0);
+      discard(offset_exp, 0);
+      discard(length_exp, 0);
+      discard(new_exp, 0);
+   }
+
+public:
+   DLLLOCAL QoreExtractOperatorNode(AbstractQoreNode *n_lvalue_exp, AbstractQoreNode *n_offset_exp,
+                                   AbstractQoreNode *n_length_exp, AbstractQoreNode *n_new_exp) : lvalue_exp(n_lvalue_exp),
+                                                                                                  offset_exp(n_offset_exp),
+                                                                                                  length_exp(n_length_exp),
+                                                                                                  new_exp(n_new_exp) {
+   }
+   DLLLOCAL virtual QoreString *getAsString(bool &del, int foff, ExceptionSink *xsink) const;
+   DLLLOCAL virtual int getAsString(QoreString &str, int foff, ExceptionSink *xsink) const;
+   // returns the type name as a c string
+   DLLLOCAL virtual const char *getTypeName() const {
+      return extract_str.getBuffer();
+   }
+
+   DLLLOCAL virtual AbstractQoreNode *parseInit(LocalVar *oflag, int pflag, int &lvids, const QoreTypeInfo *&typeInfo);
+
+   DLLLOCAL virtual bool hasEffect() const {
+      return true;
+   }
+
+   DLLLOCAL AbstractQoreNode *extract(ExceptionSink *xsink) const;
+};
+
+#endif
---
+++ lib/QoreExtractOperatorNode.cpp
@@ -0,0 +1,169 @@
+/*
+ QoreExtractOperatorNode.cpp
+ 
+ Qore Programming Language
+ 
+ Copyright 2003 - 2010 David Nichols
+ 
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+ 
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <qore/Qore.h>
+
+QoreString QoreExtractOperatorNode::extract_str("extract operator expression");
+
+// if del is true, then the returned QoreString * should be extractd, if false, then it must not be
+QoreString *QoreExtractOperatorNode::getAsString(bool &del, int foff, ExceptionSink *xsink) const {
+   del = false;
+   return &extract_str;
+}
+
+int QoreExtractOperatorNode::getAsString(QoreString &str, int foff, ExceptionSink *xsink) const {
+   str.concat(&extract_str);
+   return 0;
+}
+
+AbstractQoreNode *QoreExtractOperatorNode::evalImpl(ExceptionSink *xsink) const {
+   return extract(xsink);
+}
+
+AbstractQoreNode *QoreExtractOperatorNode::evalImpl(bool &needs_deref, ExceptionSink *xsink) const {
+   needs_deref = ref_rv;
+   return extract(xsink);
+}
+
+AbstractQoreNode *QoreExtractOperatorNode::parseInit(LocalVar *oflag, int pflag, int &lvids, const QoreTypeInfo *&typeInfo) {
+   const QoreTypeInfo *expTypeInfo = 0;
+
+   // check lvalue expression
+   lvalue_exp = lvalue_exp->parseInit(oflag, pflag | PF_FOR_ASSIGNMENT, lvids, expTypeInfo);
+   if (expTypeInfo->hasType()) {
+      if (!listTypeInfo->parseEqual(expTypeInfo)
+	  && !stringTypeInfo->parseEqual(expTypeInfo)) {
+	 QoreStringNode *desc = new QoreStringNode("the lvalue expression (1st position) with the 'extract' operator is ");
+	 expTypeInfo->getThisType(*desc);
+	 desc->sprintf(", therefore this operation is invalid and would throw an exception at run-time; the 'extract' operator only operates on lists and strings");
+	 getProgram()->makeParseException("PARSE-TYPE-ERROR", desc);
+      }
+      else
+	 typeInfo = expTypeInfo;
+   }
+
+   // check offset expression
+   expTypeInfo = 0;
+   offset_exp = offset_exp->parseInit(oflag, pflag, lvids, expTypeInfo);
+   if (expTypeInfo->nonNumericValue())
+      expTypeInfo->doNonNumericWarning("the offset expression (2nd position) with the 'extract' operator is ");
+
+   // check length expression, if any
+   if (length_exp) {
+      expTypeInfo = 0;
+      length_exp = length_exp->parseInit(oflag, pflag, lvids, expTypeInfo);
+      if (expTypeInfo->nonNumericValue())
+	 expTypeInfo->doNonNumericWarning("the length expression (3nd position) with the 'extract' operator is ");
+   }
+
+   // check new value expression, if any
+   if (new_exp)
+      new_exp = new_exp->parseInit(oflag, pflag, lvids, expTypeInfo);
+
+   return this;
+}
+
+AbstractQoreNode *QoreExtractOperatorNode::extract(ExceptionSink *xsink) const {
+   printd(5, "QoreExtractOperatorNode::extract() lvalue_exp = %p, offset_exp=%p, length_exp=%p, new_exp=%p, isEvent=%d\n", lvalue_exp, offset_exp, length_exp, new_exp, xsink->isEvent());
+
+   // evaluate arguments
+   QoreNodeEvalOptionalRefHolder eoffset(offset_exp, xsink);
+   if (*xsink)
+      return 0;
+
+   QoreNodeEvalOptionalRefHolder elength(length_exp, xsink);
+   if (*xsink)
+      return 0;
+
+   QoreNodeEvalOptionalRefHolder exp(new_exp, xsink);
+   if (*xsink)
+      return 0;
+
+   // get ptr to current value (lvalue is locked for the scope of the LValueHelper object)
+   LValueHelper val(lvalue_exp, xsink);
+   if (!val)
+      return 0;
+
+   // if value is not a list or string, throw exception
+   qore_type_t vt = val.get_type();
+
+   if (vt == NT_NOTHING) {
+      // see if the lvalue has a default type
+      const QoreTypeInfo *typeInfo = val.get_type_info();
+      if (typeInfo == listTypeInfo || typeInfo == stringTypeInfo) {
+	 if (val.assign(typeInfo->getDefaultValue()))
+	    return 0;
+	 vt = val.get_type();
+      }
+   }
+
+   if (vt != NT_LIST && vt != NT_STRING) {
+      xsink->raiseException("EXTRACT-ERROR", "first (lvalue) argument to the extract operator is not a list or a string");
+      return 0;
+   }
+   
+   // no exception can occur here
+   val.ensure_unique();
+
+   qore_size_t offset = eoffset ? eoffset->getAsBigInt() : 0;
+
+#ifdef DEBUG
+   if (vt == NT_LIST) {
+      QoreListNode *vl = reinterpret_cast<QoreListNode *>(val.get_value());
+      printd(5, "op_extract() val=%p (size=%d) offset=%d\n", val.get_value(), vl->size(), offset);
+   }
+   else {
+      QoreStringNode *vs = reinterpret_cast<QoreStringNode *>(val.get_value());
+      printd(5, "op_extract() val=%p (strlen=%d) offset=%d\n", val.get_value(), vs->strlen(), offset);
+   }
+#endif
+
+   ReferenceHolder<AbstractQoreNode> rv(xsink);
+
+   if (vt == NT_LIST) {
+      QoreListNode *vl = reinterpret_cast<QoreListNode *>(val.get_value());
+      if (!length_exp && !new_exp)
+	 rv = vl->extract(offset, xsink);
+      else {
+	 qore_size_t length = elength ? elength->getAsBigInt() : 0;
+	 if (!new_exp)
+	    rv = vl->extract(offset, length, xsink);
+	 else
+	    rv = vl->extract(offset, length, *exp, xsink);
+      }
+   }
+   else { // must be a string
+      QoreStringNode *vs = reinterpret_cast<QoreStringNode *>(val.get_value());
+      if (!length_exp && !new_exp)
+	 rv = vs->extract(offset, xsink);
+      else {
+	 qore_size_t length = elength ? elength->getAsBigInt() : 0;
+	 if (!new_exp)
+	    rv = vs->extract(offset, length, xsink);
+	 else
+	    rv = vs->extract(offset, length, *exp, xsink);
+      }
+   }
+
+   // return value only if used and no exception occured
+   return ref_rv && !*xsink ? rv.release() : 0;
+}
