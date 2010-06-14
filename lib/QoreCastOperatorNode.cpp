1d35fc63ca1858d1691a8ba965f87bf735144fa9
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2010-06-14 11:22:05 +0000

    readded html documentation to make package building easier, fixed cast operator no longer to check for class compatibility and do all checking at run-time; this allows the cast operator to work with complex class hierarchies in a more flexible way at runtime
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@3611 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- lib/QoreCastOperatorNode.cpp
+++ lib/QoreCastOperatorNode.cpp
@@ -98,9 +98,11 @@ AbstractQoreNode *QoreCastOperatorNode::parseInit(LocalVar *oflag, int pflag, in
       if (!objectTypeInfo->parseAccepts(typeInfo)) {
 	 parse_error("cast<>(%s) is invalid; cannot cast from %s to object", qc ? qc->getName() : "object", typeInfo->getName(), typeInfo->getName());
       }
+#ifdef _QORE_STRICT_CAST
       else if (qc && (qc->getTypeInfo()->parseAccepts(typeInfo) == QTI_NOT_EQUAL) && typeInfo->parseAccepts(qc->getTypeInfo()) == QTI_NOT_EQUAL) {
 	 parse_error("cannot cast from %s to %s; the classes are not compatible", typeInfo->getName(), path->ostr);
       }
+#endif
    }
 
    // free temporary memory
