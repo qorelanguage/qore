97fc4ad5b4084a06e2324c8b037b49c1c512b98d
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2012-10-15 12:26:57 +0000

    * doc updates * added a bool trim_unquoted = False option to <string>::split() with quote and split with quote to allow for trimming unquoted fields
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@5144 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- include/qore/intern/QoreLibIntern.h
+++ include/qore/intern/QoreLibIntern.h
@@ -889,7 +889,7 @@ DLLLOCAL void ignore_return_value(AbstractQoreNode* n);
 
 DLLLOCAL QoreListNode* split_intern(const char* pattern, qore_size_t pl, const char* str, qore_size_t sl, const QoreEncoding* enc, bool with_separator = false);
 DLLLOCAL QoreStringNode* join_intern(const QoreString* p0, const QoreListNode* l, int offset, ExceptionSink* xsink);
-DLLLOCAL QoreListNode* split_with_quote(const QoreString* sep, const QoreString* str, const QoreString* quote, ExceptionSink* xsink);
+DLLLOCAL QoreListNode* split_with_quote(const QoreString* sep, const QoreString* str, const QoreString* quote, bool trim_unquoted, ExceptionSink* xsink);
 DLLLOCAL bool inlist_intern(const AbstractQoreNode *arg, const QoreListNode *l, ExceptionSink *xsink);
 DLLLOCAL QoreStringNode* format_float_intern(const QoreString& fmt, double num);
 
