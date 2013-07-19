23d20bc0b8da4ffe173b56f04ce56fda6233134d
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2013-07-19 12:21:04 +0000

    major WebSocketClient updates, updated %y with binary arg to output yaml format
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@5907 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- lib/BinaryNode.cpp
+++ lib/BinaryNode.cpp
@@ -124,7 +124,12 @@ QoreString *BinaryNode::getAsString(bool &del, int foff, ExceptionSink *xsink) c
 }
 
 int BinaryNode::getAsString(QoreString &str, int foff, ExceptionSink *xsink) const {
-   str.sprintf("binary object %p ("QSD" byte%s)", getPtr(), size(), size() == 1 ? "" : "s");
+   if (foff == FMT_YAML_SHORT) {
+      QoreString bstr(this, -1);
+      str.sprintf("!!binary %s", bstr.getBuffer());
+   }
+   else
+      str.sprintf("binary object %p ("QSD" byte%s)", getPtr(), size(), size() == 1 ? "" : "s");
    return 0;
 }
 
