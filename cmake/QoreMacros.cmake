b2e3da4f45265fd81bec57f7d9beb9a9ae5bc89b
subik <subik@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-01-06 14:18:14 +0000

    cmake builds: documented; more macros introduced; compiler warning fix
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@6223 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- cmake/QoreMacros.cmake
+++ cmake/QoreMacros.cmake
@@ -1,6 +1,6 @@
 
 MACRO (QORE_WRAP_QPP _cpp_files _inputs )
-  message(STATUS "QPP: ${QORE_QPP_EXECUTABLE}")
+  #message(STATUS "QPP: ${QORE_QPP_EXECUTABLE}")
   #message(STATUS "QPP2: ${_inputs}")
 
   FOREACH (it ${_inputs})
@@ -9,18 +9,121 @@ MACRO (QORE_WRAP_QPP _cpp_files _inputs )
     GET_FILENAME_COMPONENT(_infile ${it} ABSOLUTE)
     SET(_cppfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.cpp)
     SET(_doxfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.dox.h)
-    message(STATUS "QPP processing: ${_infile} -> ${_cppfile} ${_doxfile}")
+    #message(STATUS "QPP processing: ${_infile} -> ${_cppfile} ${_doxfile}")
     ADD_CUSTOM_COMMAND(OUTPUT ${_cppfile} ${_doxfile}
       COMMAND ${QORE_QPP_EXECUTABLE}
       ARGS --output=${_cppfile} --dox-output=${_doxfile} ${_infile}
       MAIN_DEPENDENCY ${_infile}
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+      VERBATIM
       )
     SET(${_cpp_files} ${${_cpp_files}} ${_cppfile})
   ENDFOREACH (it)
 
-  message(STATUS "QPP files: ${${_cpp_files}}")
+  #message(STATUS "QPP files: ${${_cpp_files}}")
 
 ENDMACRO (QORE_WRAP_QPP)
 
 
+MACRO (QORE_BINARY_MODULE _module_name _version)
+
+    # standard repeating stuff for modules
+    add_definitions("-DPACKAGE_VERSION=\"${_version}\"")
+    include_directories( ${QORE_INCLUDE_DIR} )
+    include_directories( ${CMAKE_BINARY_DIR} )
+    
+    # compiler stuff
+    if (CMAKE_COMPILER_IS_GNUCXX)
+        SET(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -Wall")
+    endif (CMAKE_COMPILER_IS_GNUCXX)
+
+    # setup the target
+    set (_libs "")
+    foreach (value ${ARGN})
+        set(_libs "${_libs};${value}")
+    endforeach (value)
+
+    set_target_properties(${_module_name} PROPERTIES PREFIX "" SUFFIX "-api-${QORE_API_VERSION}.qmod")
+    target_link_libraries(${_module_name} ${QORE_LIBRARY} ${_libs})
+    install( TARGETS ${_module_name} DESTINATION ${QORE_MODULES_DIR})
+
+
+    # uninstall
+    if (EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in")
+        # make uninstall
+        CONFIGURE_FILE(
+            "${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in"
+            "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake"
+            IMMEDIATE @ONLY
+        )
+        ADD_CUSTOM_TARGET(uninstall
+            "${CMAKE_COMMAND}" -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_uninstall.cmake")
+
+        message(STATUS "")
+        message(STATUS "uninstall target: make uninstall")
+        message(STATUS "")
+    else()
+        message(WARNING "Uninstall script: no file: ${CMAKE_CURRENT_SOURCE_DIR}/cmake/cmake_uninstall.cmake.in")
+    endif()
+
+    # docs
+    FIND_PACKAGE(Doxygen)
+    if (DOXYGEN_FOUND)
+        if (EXISTS "${CMAKE_SOURCE_DIR}/docs/Doxyfile.in")
+            configure_file(${CMAKE_SOURCE_DIR}/docs/Doxyfile.in ${CMAKE_BINARY_DIR}/Doxyfile @ONLY)
+
+            add_custom_target(docs
+                ${DOXYGEN_EXECUTABLE} ${CMAKE_BINARY_DIR}/Doxyfile
+                WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+                COMMENT "Generating API documentation with Doxygen" VERBATIM
+            )
+            add_dependencies(docs ${_module_name})
+
+            message(STATUS "")
+            message(STATUS "documentation target: make docs")
+            message(STATUS "")
+        else()
+            message(WARNING "file does not exits: ${CMAKE_SOURCE_DIR}/docs/Doxyfile.in")
+        endif()
+
+    else (DOXYGEN_FOUND)
+        message(WARNING "Doxygen not found. Documentation won't be built")
+    endif (DOXYGEN_FOUND)
+
+
+ENDMACRO (QORE_BINARY_MODULE)
+
+
+MACRO (QORE_DIST _version)
+    # packaging related stuff
+    string(TOLOWER ${CMAKE_PROJECT_NAME} CPACK_PACKAGE_NAME)
+    SET(CPACK_PACKAGE_VERSION "${_version}")
+    SET(CPACK_SOURCE_GENERATOR "TBZ2")
+    SET(CPACK_SOURCE_PACKAGE_FILE_NAME "${CPACK_PACKAGE_NAME}-${CPACK_PACKAGE_VERSION}")
+    SET(CPACK_IGNORE_FILES "/CVS/;/\\\\.svn/;\\\\.swp$;\\\\.#;/#;\\\\.~$;\\\\.tar.gz$;/CMakeFiles/;CMakeCache.txt;refresh-copyright-and-license.pl;\\\\.spec$")
+    SET(CPACK_SOURCE_IGNORE_FILES ${CPACK_IGNORE_FILES})
+    INCLUDE(CPack)
+    # simulate autotools' "make dist"
+    add_custom_target(dist COMMAND ${CMAKE_MAKE_PROGRAM} package_source)
+
+    message(STATUS "")
+    message(STATUS "tarbal creation target: make dist")
+    message(STATUS "")
+
+ENDMACRO (QORE_DIST)
+
+MACRO (QORE_CONFIG_INFO)
+    MESSAGE(STATUS "")
+    MESSAGE(STATUS "-----------------------------")
+    MESSAGE(STATUS "System: ${CMAKE_SYSTEM}")
+    MESSAGE(STATUS "Build type: ${CMAKE_BUILD_TYPE}")
+    MESSAGE(STATUS "Prefix: ${CMAKE_INSTALL_PREFIX}")
+    IF (APPLE)
+        MESSAGE(STATUS "Archs: ${CMAKE_OSX_ARCHITECTURES}")
+    ENDIF (APPLE)
+    MESSAGE(STATUS "Compiler: ${CMAKE_CXX_COMPILER} - ${CMAKE_CXX_COMPILER_ID}")
+    MESSAGE(STATUS "FLags deb: ${CMAKE_CXX_FLAGS_DEBUG}")
+    MESSAGE(STATUS "FLags rel: ${CMAKE_CXX_FLAGS}")
+    MESSAGE(STATUS "FLags reldeb: ${CMAKE_CXX_FLAGS_RELWITHDEBINFO}")
+    MESSAGE(STATUS "-----------------------------")
+ENDMACRO (QORE_CONFIG_INFO)
