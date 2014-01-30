e1ce67fd0e86fc508fd43c91d47651d293f9aa33
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-01-30 14:23:47 +0000

    added missing functions
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/branches/0.8.8-snapshot@6268 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- lib/QoreLib.cpp
+++ lib/QoreLib.cpp
@@ -398,6 +398,17 @@ QoreAbstractIteratorBase::QoreAbstractIteratorBase() : tid(gettid()) {
 QoreAbstractIteratorBase::~QoreAbstractIteratorBase() {
 }
 
+int QoreAbstractIteratorBase::check(ExceptionSink* xsink) const {
+   if (tid != gettid()) {
+      xsink->raiseException("ITERATOR-THREAD-ERROR", "this %s object was created in TID %d; it is an error to access it from any other thread (accessed from TID %d)", getName(), tid, gettid());
+      return -1;
+   }
+   return 0;
+}
+
+QoreIteratorBase::QoreIteratorBase() {
+}
+
 FeatureList::FeatureList() {
    // register default features
    push_back("sql");
