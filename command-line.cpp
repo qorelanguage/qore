c7a0aa6c2f496e582c23305e7bb72905ad634dbf
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-07-08 22:10:16 +0000

    fixed --module-dir output since showing the path breaks module builds from source, added --module-path
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@6611 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- command-line.cpp
+++ command-line.cpp
@@ -114,7 +114,8 @@ static const char helpstr[] =
    "                               MIT; GPL modules cannot be loaded\n"
    "  -m, --show-module-errors     shows error messages related to loading and\n"
    "                               initializing qore modules\n"
-   "      --module-dir             show qore module directories and exit\n"
+   "      --module-dir             show primary qore module directory and exit\n"
+   "      --module-path            show qore module search path and exit\n"
    "      --module-api             show compatible qore module API version and\n"
    "                               exit\n"
    "      --module-apis            show all qore module API versions\n"
@@ -211,8 +212,13 @@ static void do_trace(const char *arg) {
    qore_trace = 1;
 }
 
-static void show_module_dir(const char *arg) {
+static void show_module_path(const char *arg) {
    printf("%s:%s:%s:%s\n", qore_user_module_ver_dir, qore_user_module_dir, qore_module_ver_dir, qore_module_dir);
+   exit(0);
+}
+
+static void show_module_dir(const char *arg) {
+   printf("%s\n", qore_module_dir);
    exit(0);
 }
 
@@ -631,6 +637,7 @@ static struct opt_struct_s {
    { '\0', "lgpl",                 ARG_NONE, set_lgpl },
    { '\0', "mit",                  ARG_NONE, set_mit },
    { '\0', "module-dir",           ARG_NONE, show_module_dir },
+   { '\0', "module-path",          ARG_NONE, show_module_path },
    { '\0', "short-version",        ARG_NONE, short_version },
    { '\0', "module-api",           ARG_NONE, show_module_api },
    { '\0', "module-apis",          ARG_NONE, show_module_apis },
