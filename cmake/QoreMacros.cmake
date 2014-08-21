1d7b433f9c94faf31f6f9a678e91df3463b01b6b
subik <subik@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2014-08-21 12:55:11 +0000

    cmake: new macro 'QORE_USER_MODULES' to install *.qm files into proper directory; QOreMacros are documented; removed LIB_SUFFIX detection as it's returned from 'qore' itself
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@6659 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- cmake/QoreMacros.cmake
+++ cmake/QoreMacros.cmake
@@ -1,30 +1,48 @@
+#
+# Qore Programming Languages cmake macros
+#
 
+#
+# Create C++ code from qthe QPP files
+#
+#  _ccp_files : output list of filenames created in CMAKE_CURRENT_BINARY_DIR.
+#  _inputs : input list of *.qpp files
+#
 MACRO (QORE_WRAP_QPP _cpp_files _inputs )
-  #message(STATUS "QPP: ${QORE_QPP_EXECUTABLE}")
-  #message(STATUS "QPP2: ${_inputs}")
 
-  FOREACH (it ${_inputs})
-    #message(STATUS "QPP1: ${it}")
-    GET_FILENAME_COMPONENT(_outfile ${it} NAME_WE)
-    GET_FILENAME_COMPONENT(_infile ${it} ABSOLUTE)
-    SET(_cppfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.cpp)
-    SET(_doxfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.dox.h)
-    #message(STATUS "QPP processing: ${_infile} -> ${_cppfile} ${_doxfile}")
-    ADD_CUSTOM_COMMAND(OUTPUT ${_cppfile} ${_doxfile}
-      COMMAND ${QORE_QPP_EXECUTABLE}
-      ARGS --output=${_cppfile} --dox-output=${_doxfile} ${_infile}
-      MAIN_DEPENDENCY ${_infile}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-      VERBATIM
-      )
-    SET(${_cpp_files} ${${_cpp_files}} ${_cppfile})
-  ENDFOREACH (it)
+    FOREACH (it ${_inputs})
 
-  #message(STATUS "QPP files: ${${_cpp_files}}")
+        GET_FILENAME_COMPONENT(_outfile ${it} NAME_WE)
+        GET_FILENAME_COMPONENT(_infile ${it} ABSOLUTE)
+        SET(_cppfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.cpp)
+        SET(_doxfile ${CMAKE_CURRENT_BINARY_DIR}/${_outfile}.dox.h)
+
+        ADD_CUSTOM_COMMAND(OUTPUT ${_cppfile} ${_doxfile}
+                           COMMAND ${QORE_QPP_EXECUTABLE}
+                           ARGS --output=${_cppfile} --dox-output=${_doxfile} ${_infile}
+                           MAIN_DEPENDENCY ${_infile}
+                           WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                           VERBATIM
+                        )
+        SET(${_cpp_files} ${${_cpp_files}} ${_cppfile})
+    ENDFOREACH (it)
 
 ENDMACRO (QORE_WRAP_QPP)
 
 
+# Create qore binary module.
+# Arguments:
+#  _module_name - string name of the module
+#  _version - a version. Used in -DPACKAGE_VERSION=...
+# [additional libraries] - optional list of libs
+#
+# The initial shared library is renamed to for Qore modules naming convention.
+# Linking with [additional libraries] is enabled.
+# libqore is linked automatically.
+#
+# additional targets created:
+#  'make docs' - if there is Doxygen found
+#  'make uninstall' - if exists CMAKE_CURRENT_SOURCE_DIR/cmake/cmake_uninstall.cmake.in
 MACRO (QORE_BINARY_MODULE _module_name _version)
 
     # standard repeating stuff for modules
@@ -46,7 +64,7 @@ MACRO (QORE_BINARY_MODULE _module_name _version)
     set_target_properties(${_module_name} PROPERTIES PREFIX "" SUFFIX "-api-${QORE_API_VERSION}.qmod")
     target_link_libraries(${_module_name} ${QORE_LIBRARY} ${_libs})
     # this line breaks jhbuild "chroot": install( TARGETS ${_module_name} DESTINATION ${QORE_MODULES_DIR})
-    install( TARGETS ${_module_name} DESTINATION lib${LIB_SUFFIX}/qore-modules)
+    install( TARGETS ${_module_name} DESTINATION ${QORE_MODULES_DIR})
 
 
     # uninstall
@@ -94,7 +112,17 @@ MACRO (QORE_BINARY_MODULE _module_name _version)
 
 ENDMACRO (QORE_BINARY_MODULE)
 
+# Install qore native/user modules (qm files) into proper location.
+# Example:
+#   set(QM_FILES foo.qm bar.qm)
+#   qore_user_modules(${QM_FILES})
+# Files will be installed automatically in 'make install' target
+MACRO (QORE_USER_MODULES _inputs)
+    install(FILES ${_inputs} DESTINATION ${QORE_USER_MODULES_DIR})
+ENDMACRO (QORE_USER_MODULES)
 
+
+# Make distributable (source code) tarball. Target 'make dist'
 MACRO (QORE_DIST _version)
     # packaging related stuff
     string(TOLOWER ${CMAKE_PROJECT_NAME} CPACK_PACKAGE_NAME)
@@ -113,6 +141,7 @@ MACRO (QORE_DIST _version)
 
 ENDMACRO (QORE_DIST)
 
+# prints a sumamry of configuration
 MACRO (QORE_CONFIG_INFO)
     MESSAGE(STATUS "")
     MESSAGE(STATUS "-----------------------------")
