1e7c65b7c956006187e5a42d7b597c4d92bcb675
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2015-02-06 06:52:05 +0000

    doc and copyright updates
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@7022 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- command-line.cpp
+++ command-line.cpp
@@ -7,7 +7,7 @@
 
   it should offer POSIX style command-line handling on any platform...
 
-  Copyright (C) 2003 - 2014 David Nichols
+  Copyright (C) 2003 - 2015 David Nichols
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and associated documentation files (the "Software"),
@@ -502,7 +502,7 @@ static void show_build_options(const char* arg) {
 }
 
 static void do_version(const char *arg) {
-   printf("QORE for %s %s (%d-bit build), Copyright (C) 2003 - 2014 David Nichols\n", qore_target_os, qore_target_arch, qore_target_bits);
+   printf("QORE for %s %s (%d-bit build), Copyright (C) 2003 - 2015 David Nichols\n", qore_target_os, qore_target_arch, qore_target_bits);
 
    printf("version %s", qore_version_string);
    FeatureList::iterator i = qoreFeatureList.begin();
