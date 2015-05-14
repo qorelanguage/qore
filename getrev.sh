b9ab9e24ecd6ef9047c78890d1b03b4bbe9cc9dd
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2015-05-14 11:25:15 +0000

    vastly speeded up doxygen building with bash and ksh by eliminating the call to svn or git in case of dynamically-generated qpp sources
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@7235 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- getrev.sh
+++ getrev.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# very fast check without calling exec()
+if [ -n "$BASH_VERSION" -o "$KSH_VERSION" ]; then
+    if [[ "$1" = *dox.* ]]; then 
+	echo "ignoring dox file $1"
+	exit 1
+    fi
+fi
+
 if out=$(svn info "$1" 2> /dev/null) || out=$(git svn info "$1" 2> /dev/null); then
     build=$(echo "${out}"|grep "^Revision:"|cut -f2 -d' ')
     echo "${build}"
