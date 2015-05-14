2bf5d0bda83dd8862c34cfd70405c19bfe0fce67
david_nichols <david_nichols@1b0b35fe-f740-4c1b-b404-a338b32e513b>
2015-05-14 11:25:39 +0000

    fix for last commit
    git-svn-id: svn+ssh://svn.code.sf.net/p/qore/code/qore/trunk@7236 1b0b35fe-f740-4c1b-b404-a338b32e513b

--- getrev.sh
+++ getrev.sh
@@ -2,7 +2,7 @@
 
 # very fast check without calling exec()
 if [ -n "$BASH_VERSION" -o "$KSH_VERSION" ]; then
-    if [[ "$1" = *dox.* ]]; then 
+    if [[ "$1" = *dox* ]]; then 
 	echo "ignoring dox file $1"
 	exit 1
     fi
