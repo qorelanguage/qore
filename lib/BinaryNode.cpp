d34793b07322ac01c72a97fb531eaf22f36a60ad
David Nichols <david.nichols@qoretechnologies.com>
2015-08-31 18:14:58 +0200

    fixed a bug where the bzip2 library can request a buffer size that could not be handled by the bzip2 library which is not handled by qore and causes a crash while decompressing certain input

--- lib/BinaryNode.cpp
+++ lib/BinaryNode.cpp
@@ -167,6 +167,7 @@ const char *BinaryNode::getTypeName() const {
 }
 
 int BinaryNode::preallocate(qore_size_t size) {
+   //printd(5, "BinaryNode::preallocate("QLLD") this: %p ptr: %p len: "QLLD"\n", size, this, ptr, len);
    ptr = q_realloc(ptr, size);
    if (ptr) {
       len = size;
