9fa67a1b65fcb60f9acb9c0fd99021ce5f764c48
David Nichols <david.nichols@qoretechnologies.com>
2015-09-08 11:21:35 +0200

    closes #12 fixed a bug handling closure-bound local variables when closures are created in the background operator expression added a regression test to examples/test/qore/threads/background.qtest ported background.qtest to QUnit updated QUnit to return the exception when Test::testAssertion() is called with an exception argument

--- lib/AbstractQoreNode.cpp
+++ lib/AbstractQoreNode.cpp
@@ -521,6 +521,10 @@ AbstractQoreNode* copy_and_resolve_lvar_refs(const AbstractQoreNode* n, Exceptio
       return crlr_smcall_copy(reinterpret_cast<const StaticMethodCallNode*>(n), xsink);
    else if (ntype == NT_PARSEREFERENCE)
       return reinterpret_cast<const ParseReferenceNode*>(n)->evalToIntermediate(xsink);
+   // ensure closures are evaluated in the parent thread so closure-bound local vars can be found and bound before
+   // launching the background thread (fixes https://github.com/qorelanguage/qore/issues/12)
+   else if (ntype == NT_CLOSURE)
+      return n->eval(xsink);
 
    return n->refSelf();
 }
