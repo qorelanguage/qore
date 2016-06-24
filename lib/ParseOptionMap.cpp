0cfdd6a763ee91b6187fc04cfca8fe0c1321e7d2
Ondra Tethal <ondrej.tethal@qoretechnologies.com>
2016-06-24 15:51:03 +0200

    refs #976 added parse option broken-loop-statement

--- lib/ParseOptionMap.cpp
+++ lib/ParseOptionMap.cpp
@@ -84,6 +84,7 @@ void ParseOptionMap::static_init() {
    DO_MAP("broken-logic-precedence",  PO_BROKEN_LOGIC_PRECEDENCE);
    DO_MAP("broken-int-assignments",   PO_BROKEN_INT_ASSIGNMENTS);
    DO_MAP("broken-operators",         PO_BROKEN_OPERATORS);
+   DO_MAP("broken-loop-statement",    PO_BROKEN_LOOP_STATEMENT);
 }
 
 int ParseOptionMap::find_code(const char *name) {
