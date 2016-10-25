fa7e1b0312d796e6ea360faf1e75bfe397138090
David Nichols <david.nichols@qoretechnologies.com>
2016-10-25 17:18:36 +0200

    refs #1363 fixes that allow Qorus tests to be run

--- lib/QoreObject.cpp
+++ lib/QoreObject.cpp
@@ -80,7 +80,14 @@ qore_object_private::~qore_object_private() {
 // returns true if a lock error has occurred and the transaction should be aborted or restarted; the rsection lock is held when this function is called
 bool qore_object_private::scanMembers(RSetHelper& rsh) {
    // we should never perform a scan while a call is in progress - such calls should be deferred until the last call has exited
-   assert(!call_count);
+   if (call_count) {
+      AutoLocker al(ref_mutex);
+      if (call_count) {
+         if (!scan_after_call)
+            scan_after_call = true;
+         return false;
+      }
+   }
 
    HashIterator hi(data);
    while (hi.next()) {
