4209be77a7885034c8448634ceab7225169363e7
Tomas Mandys <tomas.mandys@qoretechnologies.com>
2017-09-16 13:31:30 +0200

    get_local_vars debug output extended

--- lib/thread.cpp
+++ lib/thread.cpp
@@ -928,6 +928,7 @@ QoreHashNode* thread_get_local_vars(int frame, ExceptionSink* xsink) {
          pgm = ch->getProgram();
       }
 
+      printd(5, "thread_get_local_vars() pgmid: %d, allow-debugging: %d\n", pgm->getProgramId(), (pgm->getParseOptions64() & PO_ALLOW_DEBUGGING) != 0);
       if (!(pgm->getParseOptions64() & PO_ALLOW_DEBUGGING))
          return rv.release();
 
