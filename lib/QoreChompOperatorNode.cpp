7191216dea08713b8599e2fb0744bd33adb14af9
David Nichols <david.nichols@qoretechnologies.com>
2018-03-18 09:06:45 +0100

    refs #2706 non-scu build fix

--- lib/QoreChompOperatorNode.cpp
+++ lib/QoreChompOperatorNode.cpp
@@ -3,7 +3,7 @@
 
   Qore Programming Language
 
-  Copyright (C) 2003 - 2017 Qore Technologies, s.r.o.
+  Copyright (C) 2003 - 2018 Qore Technologies, s.r.o.
 
   Permission is hereby granted, free of charge, to any person obtaining a
   copy of this software and associated documentation files (the "Software"),
@@ -30,6 +30,7 @@
 
 #include <qore/Qore.h>
 #include "qore/intern/qore_program_private.h"
+#include "qore/intern/QoreHashNodeIntern.h"
 
 QoreString QoreChompOperatorNode::chomp_str("chomp operator expression");
 
