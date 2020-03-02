c70143238ee62975a5fc40cd5445a1625ed8fe88
David Nichols <david@qore.org>
2020-03-02 08:45:39 +0100

    removed invalid assert() that depends on random static initialization order

--- lib/QoreNothingNode.cpp
+++ lib/QoreNothingNode.cpp
@@ -3,7 +3,7 @@
 
     Qore Programming Language
 
-    Copyright (C) 2003 - 2018 Qore Technologies, s.r.o.
+    Copyright (C) 2003 - 2020 Qore Technologies, s.r.o.
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -38,7 +38,6 @@ QoreString YamlNullString("null");
 
 QoreNothingNode::QoreNothingNode() : UniqueValueQoreNode(NT_NOTHING) {
 #ifdef DEBUG
-    assert(!nothing_flag);
     nothing_flag = true;
 #endif
 }
