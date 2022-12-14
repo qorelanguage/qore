653ae435d49aed785653d0a86a9f5a203ee4e96f
David Nichols <david.nichols@qoretechnologies.com>
2022-12-14 13:32:20 +0100

    refs #4658 fixed a race condition in deadlock detection where a deadlock could occur due to the lack of atomic reads

--- include/qore/intern/VLock.h
+++ include/qore/intern/VLock.h
@@ -1,32 +1,32 @@
 /* -*- mode: c++; indent-tabs-mode: nil -*- */
 /*
- VLock.h
-  
- Qore Programming Language
- 
- Copyright (C) 2003 - 2022 David Nichols
- 
-  Permission is hereby granted, free of charge, to any person obtaining a
-  copy of this software and associated documentation files (the "Software"),
-  to deal in the Software without restriction, including without limitation
-  the rights to use, copy, modify, merge, publish, distribute, sublicense,
-  and/or sell copies of the Software, and to permit persons to whom the
-  Software is furnished to do so, subject to the following conditions:
+    VLock.h
 
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
+    Qore Programming Language
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-  DEALINGS IN THE SOFTWARE.
+    Copyright (C) 2003 - 2022 David Nichols
 
-  Note that the Qore library is released under a choice of three open-source
-  licenses: MIT (as above), LGPL 2+, or GPL 2+; see README-LICENSE for more
-  information.
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    DEALINGS IN THE SOFTWARE.
+
+    Note that the Qore library is released under a choice of three open-source
+    licenses: MIT (as above), LGPL 2+, or GPL 2+; see README-LICENSE for more
+    information.
 */
 
 #ifndef _QORE_VLOCK_H
@@ -38,43 +38,51 @@
 
 #include <vector>
 #include <map>
+#include <atomic>
 
-typedef std::map<int, class VLock *> vlock_map_t;
+typedef std::map<int, class VLock*> vlock_map_t;
 
 // testing shows that a vector is slightly faster than a deque for this usage
 // and must faster than a list
-typedef std::vector<AbstractSmartLock *> abstract_lock_list_t;
+typedef std::vector<AbstractSmartLock*> abstract_lock_list_t;
+
+// forward references
+class QoreCondition;
 
 // for tracking locks per thread and detecting deadlocks
-class VLock : protected abstract_lock_list_t
-{
-   private:
-      AbstractSmartLock *waiting_on;   // the lock this object is waiting on
-      int tid;
+class VLock : protected abstract_lock_list_t {
+public:
+    DLLLOCAL VLock(int tid);
 
-      // not implemented
-      VLock(const VLock&);
-      VLock& operator=(const VLock&);
-      
-   public:
-      DLLLOCAL VLock(int n_tid);
-      DLLLOCAL ~VLock();
-      DLLLOCAL void push(AbstractSmartLock *g);
-      DLLLOCAL int pop(AbstractSmartLock *asl);
-      DLLLOCAL void del();
-      DLLLOCAL AbstractSmartLock *find(AbstractSmartLock *g) const; 
+    DLLLOCAL ~VLock() {
+        //printd(5, "VLock::~VLock() this=%p\n", this);
+        assert(begin() == end());
+    }
 
-      // for blocking smart locks with deadlock detection
-      DLLLOCAL int waitOn(AbstractSmartLock *asl, class VLock *vl, class ExceptionSink *xsink, int timeout_ms = 0);
-      // for smart locks using an alternate condition variable
-      DLLLOCAL int waitOn(AbstractSmartLock *asl, class QoreCondition *cond, class VLock *vl, class ExceptionSink *xsink, int timeout_ms = 0);
-      // for smart locks that can be held by more than one thread
-      DLLLOCAL int waitOn(AbstractSmartLock *asl, vlock_map_t &vmap, class ExceptionSink *xsink, int timeout_ms = 0);
-      DLLLOCAL int getTID() const { return tid; }
-         
+    DLLLOCAL void push(AbstractSmartLock* g);
+    DLLLOCAL int pop(AbstractSmartLock* asl);
+    DLLLOCAL void del();
+    DLLLOCAL AbstractSmartLock* find(AbstractSmartLock* g) const;
+
+    // for blocking smart locks with deadlock detection
+    DLLLOCAL int waitOn(AbstractSmartLock* asl, VLock* vl, ExceptionSink* xsink, int timeout_ms = 0);
+    // for smart locks using an alternate condition variable
+    DLLLOCAL int waitOn(AbstractSmartLock* asl, QoreCondition* cond, VLock* vl, ExceptionSink* xsink, int timeout_ms = 0);
+    // for smart locks that can be held by more than one thread
+    DLLLOCAL int waitOn(AbstractSmartLock* asl, vlock_map_t& vmap, ExceptionSink* xsink, int timeout_ms = 0);
+    DLLLOCAL int getTID() const { return tid; }
+
 #ifdef DEBUG
-      DLLLOCAL void show(class VLock *nvl) const; 
+    DLLLOCAL void show(class VLock* nvl) const;
 #endif
+
+private:
+    std::atomic<AbstractSmartLock*> waiting_on;   // the lock this object is waiting on
+    int tid;
+
+    // not implemented
+    VLock(const VLock&);
+    VLock& operator=(const VLock&);
 };
 
 #endif
