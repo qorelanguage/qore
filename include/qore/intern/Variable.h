49f0db7708be56d7d7bdabbf6caa6ad8ca47ecb9
David Nichols <david@qore.org>
2024-05-09 15:32:21 +0200

    fixed assertion

--- include/qore/intern/Variable.h
+++ include/qore/intern/Variable.h
@@ -502,7 +502,7 @@ public:
         if (val) {
             return val->getReference();
         }
-        assert(*qv);
+        assert(qv);
         return qv->getType() == NT_REFERENCE ? qv->get<const ReferenceNode>() : nullptr;
     }
 
