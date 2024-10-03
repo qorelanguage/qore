8a53cba54024c0831e7bea5d9fc0656fa23c4ea2
David Nichols <david@qore.org>
2024-10-03 18:22:34 +0200

    removed unused code

--- lib/QoreTypeInfo.cpp
+++ lib/QoreTypeInfo.cpp
@@ -1298,50 +1298,11 @@ bool QoreTypeSpec::acceptInput(ExceptionSink* xsink, const QoreTypeInfo& typeInf
                 }
                 QoreHashNode* h = n.get<QoreHashNode>();
                 bool err = false;
-                ok = acceptInputComplexHash(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, h, err);
+                ok = acceptInputComplexHash(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, h,
+                    err);
                 if (err) {
                     return true;
                 }
-                /*
-                const QoreTypeInfo* ti = h->getValueTypeInfo();
-                if (QoreTypeInfo::equal(u.ti, ti)) {
-                    ok = true;
-                    break;
-                }
-
-                // try to fold values into our type; value types are not identical;
-                // we have to get a new hash
-                if (!h->is_unique()) {
-                    AbstractQoreNode* p = n.assign(h = qore_hash_private::get(*h)->copy(get_value_type(&typeInfo)));
-                    if (lvhelper) {
-                        lvhelper->saveTemp(p);
-                    } else {
-                        discard(p, xsink);
-                        if (xsink && *xsink) {
-                            return true;
-                        }
-                    }
-                } else {
-                    qore_hash_private::get(*h)->complexTypeInfo = &typeInfo;
-                }
-
-                // now we have to fold the value types into our type
-                HashIterator i(h);
-                while (i.next()) {
-                    hash_assignment_priv ha(*qore_hash_private::get(*h), *qhi_priv::get(i)->i);
-                    QoreValue hn(ha.swap(QoreValue()));
-                    u.ti->acceptInputIntern(xsink, arg_type, obj, param_num, param_name, hn, lvhelper);
-                    ha.swap(hn);
-                    if (xsink && *xsink) {
-                        // enrich exception so that it's not confusing
-                        xsink->appendLastDescription(" (while folding values into type 'hash<%s>')",
-                            QoreTypeInfo::getName(u.ti));
-                        return true;
-                    }
-                }
-
-                ok = true;
-                */
             } else if (t == NT_WEAKREF_HASH) {
                 if (u.ti == autoTypeInfo) {
                     ok = true;
@@ -1349,7 +1310,8 @@ bool QoreTypeSpec::acceptInput(ExceptionSink* xsink, const QoreTypeInfo& typeInf
                 }
                 QoreHashNode* h = n.get<WeakHashReferenceNode>()->get();
                 bool err = false;
-                ok = acceptInputComplexHash(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, h, err);
+                ok = acceptInputComplexHash(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, h,
+                    err);
                 if (err) {
                     return true;
                 }
@@ -1365,48 +1327,8 @@ bool QoreTypeSpec::acceptInput(ExceptionSink* xsink, const QoreTypeInfo& typeInf
                 }
                 QoreListNode* l = n.get<QoreListNode>();
                 bool err = false;
-                ok = acceptInputComplexList(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, l, err);
-                /*
-                const QoreTypeInfo* ti = l->getValueTypeInfo();
-                if (QoreTypeInfo::equal(u.ti, ti)) {
-                    ok = true;
-                    break;
-                }
-
-                // try to fold values into our type; value types are not identical;
-                // we have to get a new list
-                qore_list_private* lp;
-                if (!l->is_unique()) {
-                    AbstractQoreNode* p = n.assign(l = qore_list_private::get(*l)->copy(get_value_type(&typeInfo)));
-                    if (lvhelper) {
-                        lvhelper->saveTemp(p);
-                    } else {
-                        discard(p, xsink);
-                        if (xsink && *xsink) {
-                            return true;
-                        }
-                    }
-                    lp = qore_list_private::get(*l);
-                } else {
-                    lp = qore_list_private::get(*l);
-                    lp->complexTypeInfo = &typeInfo;
-                }
-
-                // now we have to fold the value types into our type
-                for (size_t i = 0; i < l->size(); ++i) {
-                    QoreValue ln(lp->takeExists(i));
-                    u.ti->acceptInputIntern(xsink, arg_type, obj, param_num, param_name, ln, lvhelper);
-                    lp->swap(i, ln);
-                    if (xsink && *xsink) {
-                        // enrich exception so that it's not confusing
-                        xsink->appendLastDescription(" (while folding values into type 'list<%s>')",
-                            QoreTypeInfo::getName(u.ti));
-                        return true;
-                    }
-                }
-
-                ok = true;
-                */
+                ok = acceptInputComplexList(xsink, typeInfo, arg_type, obj, param_num, param_name, n, lvhelper, l,
+                    err);
             } else if (typespec == QTS_COMPLEXSOFTLIST) {
                 // see if value matches
                 if (QoreTypeInfo::runtimeAcceptsValue(u.ti, n) > 0) {
